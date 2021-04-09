1a7660cfdd76dee3a19b6099432f52869268cadc
angelzou <angelzou@tencent.com>
2021-04-09 16:38:19 +0800

    feat: mini support css modules

--- mini/src/components/theme-page/index.jsx
+++ mini/src/components/theme-page/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 import { View } from '@tarojs/components';
-import './index.scss';
+import styles from './index.module.scss';
 
 export default inject('site')(observer((props) => {
   const { children, site } = props;
 
 
   return (
-    <View className={`dzq-page dzq-theme-${site.theme}`}>
+    <View className={`${styles['dzq-page']} dzq-theme-${site.theme}`}>
       {children}
     </View>
   );
--- mini/src/pages/index/index.jsx
+++ mini/src/pages/index/index.jsx
@@ -3,7 +3,7 @@ import { View, Button, Text } from '@tarojs/components';
 import { observer, inject } from 'mobx-react';
 import ThemePage from '@components/theme-page';
 import { APP_THEME } from '@common/constants/site';
-import './index.scss';
+import styles from './index.module.scss';
 
 @inject('site')
 @observer
@@ -22,9 +22,9 @@ class Index extends Component {
     const { envConfig, theme, changeTheme } = this.props.site;
     return (
       <ThemePage>
-        <View className='index'>
-          <Text className='text'>{envConfig.baseURL}</Text>
-          <View className='text'>{theme}</View>
+        <View>
+          <Text className={styles.text}>{envConfig.baseURL}</Text>
+          <View className={styles.text}>{theme}</View>
           <Button onClick={() => {
             changeTheme(theme === APP_THEME.light ? APP_THEME.dark : APP_THEME.light);
           }}>修改主题</Button>
