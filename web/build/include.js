568a7dce27db2100610adc1fd2b102dc0e03849a
LLLLLamHo <linhao.4zhong@163com>
2021-04-13 13:56:57 +0800

    更新文档

--- web/build/include.js
+++
@@ -1,7 +0,0 @@
-const cwd = process.cwd();
-const path = require('path');
-
-module.exports = (config) => {
-  config.module.rules[0].include.push(path.resolve(cwd, '../'));
-  return config;
-};
--- web/next.config.js
+++ web/next.config.js
@@ -1,17 +1,4 @@
 const nextConfig = require('@discuzq/cli/config/next');
-const includeFile = require('./build/include');
-const alias = require('./build/alias');
 
-// module.exports = nextConfig((config) => {
-//   config.webpack = (conf) => {
-//     let nextConfig = conf;
-
-//     // nextConfig = includeFile(nextConfig);
-//     // nextConfig = alias(nextConfig);
-
-//     return nextConfig;
-//   };
-//   return config;
-// });
 
 module.exports = nextConfig(config => config);
