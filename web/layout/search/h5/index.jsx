8aff5cc0c11c933cadd16d1f58f68edf3dbcb3ea
yzh <810593582@qq.com>
2021-04-14 17:13:47 +0800

    feat: 搜索输入框组件

--- web/layout/search/h5/index.jsx
+++ web/layout/search/h5/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import { withRouter } from 'next/router';
+
+// import SearchInput from '@components/dzp-search-input';
+import SearchInput from '.././../../components/dzp-search-input';
+
+import styles from './index.module.scss';
+import '@discuzq/design/styles/index.scss';
 
 @inject('site')
 @observer
-class SearchPCPage extends React.Component {
+class SearchH5Page extends React.Component {
+  onSearch = () => {
+    // this.props.router.push('/search-result');
+  };
+  onCancel = () => {
+    this.props.router.back();
+  };
+
   render() {
     return (
-      <div>
-        H5
+      <div className={styles.page}>
+        <SearchInput onSearch={this.onSearch} onCancel={this.onCancel} />
       </div>
     );
   }
 }
 
-export default SearchPCPage;
+export default withRouter(SearchH5Page);
