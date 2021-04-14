808028869d5c067d0c0dc2e515f3bdc08e61774e
刘宇琳 <burnellliu>
2021-04-14 17:44:44 +0800

    fix: 规范修改

--- common/components/thread/popup-list/index.jsx
+++ common/components/thread/popup-list/index.jsx
@@ -1,86 +1,79 @@
-import React, { useContext, useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Tabs, Button, Icon, Popup } from '@discuzq/design';
-import { ThreadCommonContext } from '../utils';
 import data from './data';
 import styles from './index.module.scss';
 
-const Index = ({ visible, onHidden }) => {
-    const { dispatch } = useContext(ThreadCommonContext)
+const Index = ({ visible = false, onHidden = () => {} }) => {
+  const tabList = useRef([
+    {
+      id: '0',
+      label: '全部',
+      badge: '0',
+      icon: 'succuss',
+    },
+    {
+      id: '1',
+      label: '点赞',
+      badge: '0',
+      icon: 'succuss',
+    },
+    {
+      id: '2',
+      label: '打赏',
+      badge: '0',
+      icon: 'succuss',
+    },
+  ]);
 
-    const tabList = useRef([
-        {
-            id: '0',
-            label: '全部',
-            badge: '0',
-            icon: 'succuss'
-        },
-        {
-            id: '1',
-            label: '点赞',
-            badge: '0',
-            icon: 'succuss'
-        },
-        {
-            id: '2',
-            label: '打赏',
-            badge: '0',
-            icon: 'succuss'
-        }
-    ])
+  const [dataSource, setDataSource] = useState([]);
 
-    const [dataSource, setDataSource] = useState([])
+  useEffect(() => {
+    setTimeout(() => {
+      setDataSource(data);
+    }, 1000);
+  }, []);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setDataSource(data)
-        }, 1000)
-    }, [])
-
-    const renderListItem = (item, index) => {
-        return (
-            <div className={styles.listItem} key={index}>
-                <div className={styles.wrapper}>
-                    <div className={styles.header}>
-                        <img className={styles.img} src={item.img}></img>
-                        <div className={styles.icon} style={{backgroundColor: index%2 === 0 ? '#e02433' : '#ffc300'}}>
-                            <Icon name={item.icon} />
-                        </div>
-                    </div>
-
-                    <div className={styles.content}>
-                        <span className={styles.title}>{item.name}</span>
-                        <span className={styles.subTitle}>{item.time}</span>
-                    </div>
+  const renderListItem = (item, index) => (
+    <div className={styles.listItem} key={index}>
+        <div className={styles.wrapper}>
+            <div className={styles.header}>
+                <img className={styles.img} src={item.img}></img>
+                <div className={styles.icon} style={{ backgroundColor: index % 2 === 0 ? '#e02433' : '#ffc300' }}>
+                    <Icon name={item.icon} />
                 </div>
-                
-                <Button type="primary">查看主页</Button>
             </div>
-        )
-    } 
 
-    const renderList = () => {
-        return (
-            <div className={styles.list}>
-                {dataSource.map((item, index) => renderListItem(item, index))}
+            <div className={styles.content}>
+                <span className={styles.title}>{item.name}</span>
+                <span className={styles.subTitle}>{item.time}</span>
             </div>
-        )
-    }
+        </div>
 
-    return (
-        <Popup
-            position="bottom"
-            visible={visible}
-            onClose={onHidden}
-        >
-            <Tabs>
-                {tabList.current.map(item => (
-                    <Tabs.TabPanel key={item.id} id={item.id} label={item.label} badge={item.badge} icon={item.icon}>
-                    {renderList()}
-                    </Tabs.TabPanel>
-                ))}
-            </Tabs>
-        </Popup>
-    )
-}
+        <Button type="primary">查看主页</Button>
+    </div>
+  );
 
-export default React.memo(Index) 
+  const renderList = () => (
+    <div className={styles.list}>
+        {dataSource.map((item, index) => renderListItem(item, index))}
+    </div>
+  );
+
+  return (
+    <Popup
+        position="bottom"
+        visible={visible}
+        onClose={onHidden}
+    >
+        <Tabs>
+            {tabList.current.map(item => (
+                <Tabs.TabPanel key={item.id} id={item.id} label={item.label} badge={item.badge} icon={item.icon}>
+                {renderList()}
+                </Tabs.TabPanel>
+            ))}
+        </Tabs>
+    </Popup>
+  );
+};
+
+export default React.memo(Index);
--- common/components/thread/tip/index.jsx
+++ common/components/thread/tip/index.jsx
@@ -1,30 +1,30 @@
-import React, { useContext, useState } from 'react';
-import { ThreadCommonContext } from '../utils';
+import React, { useState } from 'react';
 import PopupList from '../popup-list';
 import styles from './index.module.scss';
 
 const Index = ({ imgs = [
-    'https://gameplus-platform.cdn.bcebos.com/gameplus-platform/upload/file/img/6cf049a661ee8b72a828c951cd96bc20/6cf049a661ee8b72a828c951cd96bc20.png',
-    'https://gameplus-platform.cdn.bcebos.com/gameplus-platform/upload/file/img/6cf049a661ee8b72a828c951cd96bc20/6cf049a661ee8b72a828c951cd96bc20.png'
+  'https://gameplus-platform.cdn.bcebos.com/gameplus-platform/upload/file/img/6cf049a661ee8b72a828c951cd96bc20/6cf049a661ee8b72a828c951cd96bc20.png',
+  'https://gameplus-platform.cdn.bcebos.com/gameplus-platform/upload/file/img/6cf049a661ee8b72a828c951cd96bc20/6cf049a661ee8b72a828c951cd96bc20.png',
 ] }) => {
-    const { dispatch } = useContext(ThreadCommonContext)
-    const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
-    const onClick = () => {
-        setVisible(true)
-    }
+  const onClick = () => {
+    setVisible(true);
+  };
 
-    return (
-        <>
-            <div className={styles.wrapper} onClick={onClick} style={{width: imgs.length === 1 ? '0.24rem' : '0.44rem'}}>
-                {
-                    imgs.map((item, index) => <img className={index === 1 ? styles.img : styles.imgAfter} src={item} key={index} />)
-                }
-            </div>
-            
-            <PopupList visible={visible} onHidden={() => setVisible(false)} />
-        </>
-    )
-}
+  return (
+    <>
+        <div className={styles.container} onClick={onClick} style={{ width: imgs.length === 1 ? '0.24rem' : '0.44rem' }}>
+            {
+                imgs.map((item, index) => (
+                    <img className={index === 1 ? styles.img : styles.imgAfter} src={item} key={index} />
+                ))
+            }
+        </div>
 
-export default React.memo(Index) 
+        <PopupList visible={visible} onHidden={() => setVisible(false)} />
+    </>
+  );
+};
+
+export default React.memo(Index);
