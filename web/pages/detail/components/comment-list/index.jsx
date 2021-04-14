ce56c8f5a8b028ec0e0264bd310eb3e8e0eeb8e6
Lizhen.Cui <927117360@qq.com>
2021-04-14 21:55:25 +0800

    feat: 添加详情页评论

---
+++ web/pages/detail/components/comment-list/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './index.module.scss';
+import { Avatar } from '@discuzq/design';
+import '@discuzq/design/styles/index.scss';
+
+export default class CommentList extends React.Component {
+  static async getInitialProps() {
+    return {
+
+    };
+  }
+  // 点赞和回复
+  handleClik = (type) => {
+    console.log(type);
+    if (type === '1') {
+      console.log('点赞');
+    } else {
+      console.log('回复');
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.commentList}>
+        <div className={styles.commentListAvatar}>
+          <Avatar image={this.props.data.avatar} circle={true}></Avatar>
+        </div>
+        <div className={styles.commentListContent}>
+          <div className={styles.commentListContentText}>
+            <div className={styles.commentListName}>
+              {this.props.data.userName}
+            </div>
+            <div className={styles.commentListText}>
+              {this.props.data.content}
+            </div>
+          </div>
+          <div className={styles.commentListFooter}>
+            <div className={styles.commentTime}>{3}分钟</div>
+            <div className={styles.commentLiked} onClick={() => this.handleClik('1')}>赞</div>
+            <div className={styles.commentReply} onClick={() => this.handleClik('2')}>回复</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- web/pages/detail/index.jsx
+++ web/pages/detail/index.jsx
@@ -1,17 +1,129 @@
 import React from 'react';
 import styles from './index.module.scss';
+// import { Icon } from '@discuzq/design';
+import '@discuzq/design/styles/index.scss';
+import CommentList from './components/comment-list/index';
+import readThreadDetail from '../../../common/server/readThreadDetail';
 export default class Detail extends React.Component {
   static async getInitialProps() {
     return {
 
     };
   }
+  state = {
+    commentSort: true,
+    commentData: [
+      {
+        userName: '张三',
+        content: '内容你内容内容内容你内容内容内',
+        avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F13%2F20170513155641_wCyQ2.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620980557&t=dce708a36610fb346866dc45ed90bba7',
+      },
+      {
+        userName: '张三',
+        content: '内容你内容内容内容你内容内容内',
+        avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F13%2F20170513155641_wCyQ2.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620980557&t=dce708a36610fb346866dc45ed90bba7',
+      },
+      {
+        userName: '张三',
+        content: '内容你内容内容内容你内容内容内',
+        avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F13%2F20170513155641_wCyQ2.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620980557&t=dce708a36610fb346866dc45ed90bba7',
+      },
+      {
+        userName: '张三',
+        content: '内容你内容内容内容你内容内容内',
+        avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F13%2F20170513155641_wCyQ2.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620980557&t=dce708a36610fb346866dc45ed90bba7',
+      },
+      {
+        userName: '张三',
+        content: '内容你内容内容内容你内容内容内',
+        avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F13%2F20170513155641_wCyQ2.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620980557&t=dce708a36610fb346866dc45ed90bba7',
+      },
+      {
+        userName: '张三',
+        content: '内容你内容内容内容你内容内容内',
+        avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F13%2F20170513155641_wCyQ2.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620980557&t=dce708a36610fb346866dc45ed90bba7',
+      },
+      {
+        userName: '张三',
+        content: '内容你内容内容内容你内容内容内',
+        avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F13%2F20170513155641_wCyQ2.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620980557&t=dce708a36610fb346866dc45ed90bba7',
+      },
+    ],
+    commentItem: {
+      userName: '张三',
+      content: '内容你内容内容内容你内容内容内',
+      avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F13%2F20170513155641_wCyQ2.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620980557&t=dce708a36610fb346866dc45ed90bba7',
+    },
+  }
+  componentDidMount(e) {
+    console.log(e);
+    this.readThreadDetail();
+  }
+  // 评论列表排序
+  commentSort = () => {
+    console.log('评论列表排序');
+    this.setState({
+      commentSort: !this.state.commentSort,
+    });
+  }
+  // 写评论
+  createComment = () => {
+    console.log('写评论');
+  }
+  // 点击详情页底部icon
+  footerIconClick = (type) => {
+    console.log(type);
+    if (type === '1') {
+      console.log('收藏');
+    } else if (type === '2') {
+      console.log('分享');
+    } else {
+      console.log('定位到评论区第一条');
+    }
+  }
+  // 请求帖子详情
+  readThreadDetail = () => {
+    const param = {
+      pid: 1,
+    };
+    const result = readThreadDetail(param);
+    console.log(result);
+  }
 
   render() {
     return (
-      <div className='index'>
-        <h1>detail</h1>
-        <p className={styles.text}>123123123</p>
+      <div className={styles.index}>
+        <div className={styles.content}>内容</div>
+        <div className={styles.comment}>
+          <div className={styles.commentHeader}>
+            <div className={styles.commentNum}>
+              共{1}条评论
+            </div>
+            <div className={styles.commentSort} onClick={this.commentSort}>
+              {
+                this.state.commentSort ? '评论从旧到新' : '评论从新到旧'
+              }
+            </div>
+          </div>
+          <div className={styles.commentItems}>
+            {
+              this.state.commentData.map((val, index) => <CommentList data={val} key={index}></CommentList>)
+            }
+          </div>
+        </div>
+        <div className={styles.detailFooter}>
+          <div className={styles.createComment}>
+            <p className={styles.createCommentText} onClick={this.createComment}>
+              写评论
+            </p>
+          </div>
+          <div className={styles.detailIcon}>
+            {/* <Icon name={'comment'}></Icon> */}
+            <div className={styles.footerIcon} onClick={() => this.footerIconClick('0')}>图标1</div>
+            <div className={styles.footerIcon} onClick={() => this.footerIconClick('1')}>图标2</div>
+            <div className={styles.footerIcon} onClick={() => this.footerIconClick('2')}>图标3</div>
+          </div>
+        </div>
       </div>
     );
   }
