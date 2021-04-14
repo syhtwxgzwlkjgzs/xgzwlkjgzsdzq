eea4fe12d5bd85eeaa748f7b55493a8563153855
刘宇琳 <burnellliu>
2021-04-14 22:34:51 +0800

    feat: 整合公共组件

--- common/components/thread/bottom-event/index.jsx
+++ common/components/thread/bottom-event/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import style from './Index.module.scss'
-import Tip from '../tip'
+import style from './Index.module.scss';
+import Tip from '../tip';
 
 /**
  * 帖子底部内容
@@ -9,12 +9,7 @@ import Tip from '../tip'
  * @prop {number}    comment 评论人数
  * @prop {number}    sharing 分享人数
  */
-const Index = ({userImas = {}, wholeNum = 11, comment = 1, sharing= 0}) => {
-  const datas = [
-    'https://gameplus-platform.cdn.bcebos.com/gameplus-platform/upload/file/img/6cf049a661ee8b72a828c951cd96bc20/6cf049a661ee8b72a828c951cd96bc20.png',
-    'https://gameplus-platform.cdn.bcebos.com/gameplus-platform/upload/file/img/6cf049a661ee8b72a828c951cd96bc20/6cf049a661ee8b72a828c951cd96bc20.png',
-  ]
-  return <div className={style.user}>
+const Index = ({ userImas = {}, wholeNum = 11, comment = 1, sharing = 0, datas = [] }) => <div className={style.user}>
     <div className={style.userImg}>
       <dev className={style.portrait}>
         <Tip imgs={datas}></Tip>
@@ -28,6 +23,5 @@ const Index = ({userImas = {}, wholeNum = 11, comment = 1, sharing= 0}) => {
       {comment > 0 && sharing > 0 && <p className={style.division}>·</p>}
       {sharing > 0 && <p className={style.commentNum}>{`${sharing}次分享`}</p>}
     </div>
-  </div>
-}
+  </div>;
 export default React.memo(Index);
