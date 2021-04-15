ad26cb8987c88e6dbb41c4f7f403d7bcfe98148d
刘宇琳 <burnellliu>
2021-04-15 18:17:37 +0800

    fix: 更新附件组件

--- common/components/thread/attachment-view/index.jsx
+++ common/components/thread/attachment-view/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './index.module.scss';
 import { Icon } from '@discuzq/design';
+import { extensionList } from './utils';
 
 /**
  * 附件
@@ -11,17 +12,25 @@ import { Icon } from '@discuzq/design';
 const Index = ({ attachments = [], isHidden = true }) => (
     <div>
         {
-          attachments.map(item => (
-            <div className={styles.container}>
-              <div>
-                <Icon name="PaperClipOutlined" />
-                <span className={styles.content}>{item.name}</span>
-                <span className={styles.size}>{item.size}</span>
-              </div>
+          attachments.map((item, index) => {
+            // 获取文件类型
+            const extension = item.name.split('.')[item.name.split('.').length - 1];
+            const type = extensionList.indexOf(extension.toUpperCase()) > 0
+              ? extension.toUpperCase()
+              : 'UNKNOWN';
+            return (
+              <div className={styles.container} key={index}>
+                <div>
+                  {/* TODO 此处逻辑接口确定之后再改 */}
+                  <Icon name={type && 'PaperClipOutlined'} />
+                  <span className={styles.content}>{item.name}</span>
+                  <span className={styles.size}>{item.size}</span>
+                </div>
 
-              {!isHidden && <Icon name="CloseOutlined" />}
-            </div>
-          ))
+                {!isHidden && <Icon name="CloseOutlined" />}
+              </div>
+            );
+          })
         }
     </div>
 );
