5d66c162e72b9d757474ace27e8b6c36dcbb4f1c
刘宇琳 <burnellliu>
2021-04-17 23:19:18 +0800

    feat: 完成帖子模拟数据

--- web/components/thread/attachment-view/index.jsx
+++ web/components/thread/attachment-view/index.jsx
@@ -9,12 +9,24 @@ import { extensionList, noop } from '../utils';
  * @prop {Boolean} isHidden 是否隐藏删除按钮
  */
 
-const Index = ({ attachments = [], isHidden = true, onClick = noop }) => (
+const Index = ({ attachments = [], isHidden = true, onClick = noop }) => {
+  // 处理文件大小的显示
+  const handleFileSize = (fileSize) => {
+    if (fileSize > 1000000) {
+      return `${(fileSize / 1000000).toFixed(2)} M`;
+    }
+    if (fileSize > 1000) {
+      return `${(fileSize / 1000).toFixed(2)} KB`;
+    }
+
+    return `${fileSize} B`;
+  };
+  return (
     <div>
         {
           attachments.map((item, index) => {
             // 获取文件类型
-            const extension = item.name.split('.')[item.name.split('.').length - 1];
+            const extension = item.fileName.split('.')[item.fileName.split('.').length - 1];
             const type = extensionList.indexOf(extension.toUpperCase()) > 0
               ? extension.toUpperCase()
               : 'UNKNOWN';
@@ -23,8 +35,8 @@ const Index = ({ attachments = [], isHidden = true, onClick = noop }) => (
                 <div>
                   {/* TODO 此处逻辑接口确定之后再改 */}
                   <Icon name={type && 'PaperClipOutlined'} />
-                  <span className={styles.content}>{item.name}</span>
-                  <span className={styles.size}>{item.size}</span>
+                  <span className={styles.content}>{item.fileName}</span>
+                  <span className={styles.size}>{handleFileSize(parseFloat(item.fileSize || 0))}</span>
                 </div>
 
                 {!isHidden && <Icon name="CloseOutlined" />}
@@ -33,6 +45,7 @@ const Index = ({ attachments = [], isHidden = true, onClick = noop }) => (
           })
         }
     </div>
-);
+  );
+};
 
 export default React.memo(Index);
