0b4996a014000c854cf352a35568b3c726619516
angelzou <angelzou@tencent.com>
2021-04-17 10:00:07 +0800

    feat: 增加附件上传的接口,更新v3接口的请求链接

--- common/server/thread-post/read-emoji.js
+++ common/server/thread-post/read-emoji.js
@@ -4,6 +4,6 @@ import api from '../api';
  * 获取表情
  */
 export default async function readEmoji() {
-  const res = await api.readEmoji();
+  const res = await api.readEmoji({ url: '/apiv3/emoji' });
   return res;
 }
