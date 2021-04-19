e7c85cac63a1942bd6fe0d9203fb2b877856a3a6
刘宇琳 <burnellliu>
2021-04-19 22:21:39 +0800

    feat: 调试发现模块接口

--- common/server/readTopicsList.js
+++
@@ -1,6 +0,0 @@
-import api from './api';
-// 潮流话题
-export default async function readTopicsList(opts, ctx = null) {
-  const res = await api.readTopicsList({ ...opts, __context: ctx });
-  return res;
-}
