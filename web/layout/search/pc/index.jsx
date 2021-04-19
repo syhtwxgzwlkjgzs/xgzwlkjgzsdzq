c81737938971d60b4ecdb9596d170cfbe35f235f
刘宇琳 <burnellliu>
2021-04-19 18:57:17 +0800

    feat: PC发现模块第一次提交

--- web/layout/search/pc/index.jsx
+++ web/layout/search/pc/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import ThreadContent from '@components/thread';
 
 @inject('site')
 @observer
 class SearchPCPage extends React.Component {
   render() {
-    return <div>PC</div>;
+    return <ThreadContent />;
   }
 }
 
