31126d5ee17ceb22604f6dc95413febc74d1572c
lijunjun <li13754950174@163.com>
2021-04-21 11:34:12 +0800

    feat: pc首页组件展示

--- web/layout/index/pc/index.jsx
+++ web/layout/index/pc/index.jsx
@@ -3,19 +3,34 @@ import { inject, observer } from 'mobx-react';
 import styles from './index.module.scss';
 import BaseLayout from '@components/base-layout';
 import ThreadContent from '@components/thread';
+import Navigation from './components/navigation';
+import QcCode from './components/qcCode';
+import Recommend from './components/recommend';
 
 @inject('site')
 @inject('user')
 @inject('index')
 @observer
 class IndexPCPage extends React.Component {
+  changeBatch = () => {
+    console.log('换一批');
+  }
+  recommendDetails = () => {
+    console.log('推荐详情');
+  }
   render() {
     const { index, user, site } = this.props;
     return (
       <div>
         <BaseLayout
-          left={() => <div>左边</div>}
-          right={() => <div>右边</div>}
+          left={() => <div><Navigation /></div>}
+          right={() => <div>
+            <QcCode />
+            <Recommend
+              changeBatch={this.changeBatch}
+              recommendDetails={this.recommendDetails}
+            />
+          </div>}
         >
           {() => <ThreadContent />}
         </BaseLayout>
