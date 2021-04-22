d912a074c277ef4e0e95c9a069bfe0bc2402bbd9
刘宇琳 <burnellliu>
2021-04-22 18:05:15 +0800

    fix: 首页数据容错

--- web/layout/index/h5/index.jsx
+++ web/layout/index/h5/index.jsx
@@ -38,7 +38,7 @@ class IndexH5Page extends React.Component {
     });
   }
 
-  onClickTab = (id) => {
+  onClickTab = (id = '') => {
     const { dispatch = () => {} } = this.props;
     dispatch('click-filter', { categoryids: [id] });
 
@@ -91,27 +91,31 @@ class IndexH5Page extends React.Component {
           themeNum={countThreads}
         />
         <div className={styles.homeContent}>
-          <Tabs
-            scrollable
-            type='primary'
-            onActive={this.onClickTab}
-            activeId={currentIndex}
-            tabBarExtraContent={
-              <div onClick={this.searchClick} className={styles.tabIcon}>
-                <Icon name="SecondaryMenuOutlined" />
-              </div>
-            }
-          >
-            {
-              categories.map((item, index) => (
-                <Tabs.TabPanel
-                  key={index}
-                  id={item.pid}
-                  label={item.name}
-                />
-              ))
-            }
-          </Tabs>
+          {
+            categories && (
+              <Tabs
+                scrollable
+                type='primary'
+                onActive={this.onClickTab}
+                activeId={currentIndex}
+                tabBarExtraContent={
+                  <div onClick={this.searchClick} className={styles.tabIcon}>
+                    <Icon name="SecondaryMenuOutlined" />
+                  </div>
+                }
+              >
+                {
+                  categories?.map((item, index) => (
+                    <Tabs.TabPanel
+                      key={index}
+                      id={item.pid}
+                      label={item.name}
+                    />
+                  ))
+                }
+              </Tabs>
+            )
+          }
         </div>
         <div className={styles.homeContent}>
           <TopNew data={sticks}/>
