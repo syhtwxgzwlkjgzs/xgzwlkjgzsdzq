c2f344e070f8b7bfa1fa5fecf388f6c573d4418c
fishcui <fishcui@tencent.com>
2021-04-22 17:35:07 +0800

    fix: 两处显示细节修复

--- web/layout/user/h5/wx-select/index.jsx
+++ web/layout/user/h5/wx-select/index.jsx
@@ -9,7 +9,6 @@ import { get } from '../../../../../common/utils/get';
 import setAccessToken from '../../../../../common/utils/set-access-token';
 import { usernameAutoBind } from '@server';
 
-
 @inject('site')
 @inject('user')
 @observer
@@ -19,63 +18,77 @@ class WXSelectH5Page extends React.Component {
     const { sessionToken, nickname } = router.query;
     return (
       <div className={layout.container}>
-          <HeaderLogin/>
-          <div className={layout.content}>
-              <div className={layout.title}>绑定微信号</div>
-              <div className={layout.tips}>
-              微信用户 {nickname}，请选择您要进行的操作
-              </div>
-              <Button className={layout.button} type="primary" onClick={async () => {
-                try {
-                  const res = await usernameAutoBind({
-                    timeout: 10000,
-                    params: {
-                      sessionToken,
-                    },
+        <HeaderLogin />
+        <div className={layout.content}>
+          <div className={layout.title}>绑定微信号</div>
+          <div className={layout.tips}>微信用户 {nickname}，请选择您要进行的操作</div>
+          <Button
+            className={layout.button}
+            type="primary"
+            onClick={async () => {
+              try {
+                const res = await usernameAutoBind({
+                  timeout: 10000,
+                  params: {
+                    sessionToken,
+                  },
+                });
+                Toast.success({
+                  content: res.code + res.msg,
+                });
+                if (res.code === 0) {
+                  const accessToken = get(res, 'data.accessToken', '');
+                  // 种下 access_token
+                  setAccessToken({
+                    accessToken,
                   });
-                  Toast.success({
-                    content: res.code + res.msg,
-                  });
-                  if (res.code === 0) {
-                    const accessToken = get(res, 'data.accessToken', '');
-                    // 种下 access_token
-                    setAccessToken({
-                      accessToken,
-                    });
 
-                    this.props.router.push('/index');
+                  this.props.router.push('/index');
 
-                    // TODO: 处理中间状态
-                    return;
-                  }
-                  throw {
-                    Code: res.code,
-                    Message: res.msg,
-                  };
-                } catch (error) {
-                  if (error.Code) {
-                    throw error;
-                  }
-                  throw {
-                    Code: 'ulg_9999',
-                    Message: '网络错误',
-                    error,
-                  };
+                  // TODO: 处理中间状态
+                  return;
                 }
-              }}>
-                微信登录
-              </Button>
-              <Button className={layout.button} type="primary" onClick={() => {
+                throw {
+                  Code: res.code,
+                  Message: res.msg,
+                };
+              } catch (error) {
+                if (error.Code) {
+                  throw error;
+                }
+                throw {
+                  Code: 'ulg_9999',
+                  Message: '网络错误',
+                  error,
+                };
+              }
+            }}
+          >
+            微信登录
+          </Button>
+          {this.props.site.isUserLoginVisible && (
+            <Button
+              className={layout.button}
+              type="primary"
+              onClick={() => {
                 router.push({ pathname: 'wx-bind-username', query: { sessionToken, nickname } });
-              }}>
-                使用用户名密码登录，并绑定微信
-              </Button>
-              <Button className={layout.button} type="primary" onClick={() => {
+              }}
+            >
+              使用用户名密码登录，并绑定微信
+            </Button>
+          )}
+          {this.props.site.isSmsOpen && (
+            <Button
+              className={layout.button}
+              type="primary"
+              onClick={() => {
                 router.push({ pathname: 'wx-bind-phone', query: { sessionToken, nickname } });
-              }}>
-                使用手机号登录，并绑定微信
-              </Button>
-          </div>
+              }}
+            >
+              使用手机号登录，并绑定微信
+            </Button>
+          )}
+        </div>
       </div>
     );
   }
