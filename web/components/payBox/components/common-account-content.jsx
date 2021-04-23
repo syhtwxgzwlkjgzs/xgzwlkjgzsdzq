befac7fe9f73221fab14e4cfc8897f5634cfaac9
yzh <810593582@qq.com>
2021-04-23 11:40:35 +0800

    '修改确认金额结构以及样式调整'

--- web/components/payBox/components/common-account-content.jsx
+++ web/components/payBox/components/common-account-content.jsx
@@ -27,29 +27,42 @@ export default class CommonAccountContent extends Component {
   };
 
   render() {
-    const { currentPaymentObj = {}, isNotShowTitle, titleName } = this.props;
-    const { trade_type, goods_name, is_anonymous, pay_money } = currentPaymentObj;
+    const { currentPaymentData = [], isNotShowTitle, titleName } = this.props;
     return (
       <>
         {/* 标题 */}
         {!isNotShowTitle && <div className={styles.amountTitle}>{titleName}</div>}
         {/* 主要内容区域 */}
         <div className={styles.amountContent}>
-          <div className={styles.acExplain}>
-            <span className={styles.acExplain_label}>交易类型</span>{' '}
-            <span className={styles.acExplain_value}>{this.renderDiffTradeType(trade_type)}</span>
-          </div>
-          <div className={styles.acExplain}>
-            <span className={styles.acExplain_label}>商品名称</span>{' '}
-            <span className={styles.acExplain_value}>{goods_name}</span>
-          </div>
-          <div className={styles.acExplain}>
-            <span className={styles.acExplain_label}>支付金额</span>
-            <span>￥{pay_money}</span>
-          </div>
-          <div className={styles.acExplain}>
-            <Checkbox checked={is_anonymous == '1'} /> 隐藏我的付费信息
-          </div>
+          {currentPaymentData &&
+            !!currentPaymentData.length &&
+            currentPaymentData.map((item, index) => {
+              let is_show_hr = currentPaymentData.length > 0 && index != currentPaymentData.length - 1;
+              return (
+                <>
+                  <div className={styles.acExplain}>
+                    <span className={styles.acExplain_label}>交易类型</span>{' '}
+                    <span className={styles.acExplain_value}>{this.renderDiffTradeType(item.trade_type)}</span>
+                  </div>
+                  <div className={styles.acExplain}>
+                    <span className={styles.acExplain_label}>商品名称</span>{' '}
+                    <span className={styles.acExplain_value}>{item.goods_name}</span>
+                  </div>
+                  <div className={styles.acExplain}>
+                    <span className={styles.acExplain_label}>支付金额</span>
+                    <span>￥{item.pay_money}</span>
+                  </div>
+                  <div className={styles.acExplain}>
+                    <Checkbox checked={item.is_anonymous == '1'} /> 隐藏我的付费信息
+                  </div>
+                  {is_show_hr && (
+                    <div>
+                      <hr className={styles.acExplain_hr}/>
+                    </div>
+                  )}
+                </>
+              );
+            })}
         </div>
       </>
     );
@@ -57,7 +70,7 @@ export default class CommonAccountContent extends Component {
 }
 
 CommonAccountContent.defaultProps = {
-  currentPaymentObj: {}, // 当前支付对象
+  currentPaymentData: [], // 当前支付对象
   isNotShowTitle: false, // 是否不显示title标题
   titleName: '确认金额', // 默认交易金额title标题名称
 };
--- web/components/payBox/h5/amount-recognized/index.jsx
+++ web/components/payBox/h5/amount-recognized/index.jsx
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
 import { Popup, Icon, Button, Checkbox } from '@discuzq/design';
 import styles from './index.module.scss';
-import CommonAccountContent from '../../components/common-account-content'
+import CommonAccountContent from '../../components/common-account-content';
 
 export default class AmountRecognized extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isShow: false,
-      currentPaymentObj: {
-        trade_type: '1', // 交易类型 1|2|3 帖子|打赏|付费加入
-        goods_name: '帖子标题', // 商品名称
-        pay_money: '9.90', // 支付金额
-        is_anonymous: '1', // 是否匿名
-      },
-      // {
-      //   trade_type: '2',
-      //   goods_name: '打赏的内容', // 商品名称
-      //   pay_money: '19.90', // 支付金额
-      //   is_anonymous: '0', // 是否匿名
-      // },
-      // {
-      //   trade_type: '3',
-      //   goods_name: '付费加入...', // 商品名称
-      //   pay_money: '19.90', // 支付金额
-      //   is_anonymous: '0', // 是否匿名
-      // },
+      currentPaymentData: [
+        {
+          trade_type: '1', // 交易类型 1|2|3 帖子|打赏|付费加入
+          goods_name: '帖子标题', // 商品名称
+          pay_money: '9.90', // 支付金额
+          is_anonymous: '1', // 是否匿名
+        },
+        {
+          trade_type: '2',
+          goods_name: '打赏的内容', // 商品名称
+          pay_money: '19.90', // 支付金额
+          is_anonymous: '0', // 是否匿名
+        },
+        {
+          trade_type: '3',
+          goods_name: '付费加入...', // 商品名称
+          pay_money: '19.90', // 支付金额
+          is_anonymous: '0', // 是否匿名
+        }
+      ],
     };
   }
 
@@ -56,19 +58,17 @@ export default class AmountRecognized extends Component {
   };
 
   // 点击支付去到
-  goToThePayConfirmPage = () => {
-
-  }
+  goToThePayConfirmPage = () => {};
 
   render() {
-    const { currentPaymentObj = {} } = this.state;
+    const { currentPaymentData = [] } = this.state;
     return (
       <Popup position="bottom" maskClosable={true} visible={this.state.isShow}>
         <div className={styles.amountWrapper}>
-          <CommonAccountContent currentPaymentObj={currentPaymentObj}/>
+          <CommonAccountContent currentPaymentData={currentPaymentData} />
           {/* 按钮区域-提交内容 */}
           <div className={styles.amountSubmit}>
-            <Button type="primary" onClick ={this.goToThePayConfirmPage} size="large" className={styles.asBtn} full>
+            <Button type="primary" onClick={this.goToThePayConfirmPage} size="large" className={styles.asBtn} full>
               支付 ￥...
             </Button>
           </div>
