28338d4c9cb6eac3ecc0f6eae7c24bb36989fd3b
yzh <810593582@qq.com>
2021-04-25 20:31:30 +0800

    '小程序输入支付密码页面完成'

--- mini/src/pages/test/paybox/index.jsx
+++ mini/src/pages/test/paybox/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PayBox from '../../../components/payBox/amount-recognized'
+import PayPwd from '../../../components/payPwd/index'
 import { Button, View } from '@tarojs/components';
 
 export default function index() {
   return (
     <View>
       <PayBox />
+      {/* <PayPwd /> */}
     </View>
   )
 }
