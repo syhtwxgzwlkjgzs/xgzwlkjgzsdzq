dd4de9dd39876863801283892010ae3a5edaa02f
fishcui <fishcui@tencent.com>
2021-04-26 18:09:11 +0800

    feat: pc增加了付费的清空逻辑

--- web/components/payBox/pc/index.jsx
+++ web/components/payBox/pc/index.jsx
@@ -3,16 +3,20 @@ import { inject, observer } from 'mobx-react';
 import { STEP_MAP } from '@common/constants/payBoxStoreConstants';
 import AmountRecognized from './amount-recognized';
 import PayConfirmed from './pay-confirmed';
-import { Dialog, Button, Checkbox } from '@discuzq/design';
+import { Dialog } from '@discuzq/design';
 
 @inject('payBox')
 @observer
 class PayBoxPc extends React.Component {
   render() {
-    return (<Dialog  visible={this.props.payBox.visible} position="center" maskClosable={true}>
+    return (
+      <Dialog visible={this.props.payBox.visible} position="center" maskClosable={true} onClose={() => {
+        this.props.payBox.clear();
+      }}>
         {this.props.payBox.step === STEP_MAP.SURE && <AmountRecognized />}
         {this.props.payBox.step === STEP_MAP.PAYWAY && <PayConfirmed />}
-    </Dialog>);
+      </Dialog>
+    );
   }
 }
 
