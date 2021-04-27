89d96d23ba2fedb5754907e56188c3eb1d340f60
angelzou <angelzou@tencent.com>
2021-04-27 17:05:23 +0800

    feat: 编辑器兼容pc

---
+++ web/components/editor/options.js
@@ -0,0 +1,31 @@
+// 基础的工具栏
+export const baseToolbar = [
+  {
+    name: 'bold',
+    icon: '<div class="dzq-icon dzq-icon-MakeSthOutlined" style="font-size:16px"></div>',
+  },
+  {
+    name: 'italic',
+    icon: '<div class="dzq-icon dzq-icon-BiasOutlined" style="font-size:16px"></div>',
+  },
+  {
+    name: 'strike',
+    icon: '<div class="dzq-icon dzq-icon-CentralLineOutlined" style="font-size:16px"></div>',
+  },
+];
+
+// 编辑器基础的选项配置
+export const baseOptions = {
+  mode: 'wysiwyg',
+  height: 178,
+  placeholder: '请填写您的发布内容…',
+  cache: { enable: false },
+  toolbar: [...baseToolbar],
+  toolbarConfig: {
+    hide: false,
+    pin: true,
+  },
+  outline: {
+    enable: false,
+  },
+};
