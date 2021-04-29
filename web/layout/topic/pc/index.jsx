27be58f7c3741f97e40ed6304f0ba95b39b7c84c
lixiaoxia <2416388215@qq.com>
2021-04-29 11:27:25 +0800

    feat: 话题列表和话题详情

--- web/layout/topic/pc/index.jsx
+++ web/layout/topic/pc/index.jsx
@@ -1,18 +1,81 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import { withRouter } from 'next/router';
 import styles from './index.module.scss';
-
+import BaseLayout from '@components/base-layout';
+import SectionTitle from '@layout/search/h5/components/section-title'
+import TrendingTopicMore from '@layout/search/pc/components/trending-topic-more';
+import ActiveUsers from '@layout/search/pc/components/active-users'
+import List from '@components/list'
 @inject('site')
 @inject('user')
-@inject('index')
+@inject('topic')
 @observer
 class IndexPCPage extends React.Component {
-  render() {
+  state = {
+    keyword: '',
+    sort: ''
+  }
+  redirectToSearchResultUser = () => {
+    this.props.router.push('/search/result-user');
+  };
+  onTopicClick = data => console.log('topic click', data);
+  fetchMoreData = () => {
+    const { dispatch } = this.props;
+    const { keyword } = this.state;
+    return dispatch('moreData', keyword);
+  };
+  onSearch = (keyword = '') => {
+    this.setState({
+      keyword
+    })
+    const { dispatch } = this.props;
+    return dispatch('refresh', { keyword, sort: this.state.sort });
+  };
+   // 右侧 - 步骤条
+   renderRight = () => {
+     console.log(this.props.search);
+    // const { pageData = [] } = this.props.search.indexUsers || { pageData: [] };
     return (
-      <div className={styles.container}>
-       话题
+      <>
+      {
+        // pageData?.length > 0 && (
+          <div className={styles.searchRight}>
+            <div className={styles.section}>
+              <SectionTitle title="活跃用户" onShowMore={this.redirectToSearchResultUser}/>
+              {/* <ActiveUsers data={pageData} onItemClick={this.onUserClick}/> */}
+            </div>
+          </div>
+        // )
+      }
+      </>
+    )
+  }
+  // 中间 -- 潮流话题 活跃用户 热门内容
+  renderContent = () => {
+    // const { pageData = [] } = this.props.search.topics || { pageData: [] };
+    const { pageData = [], currentPage = 0, totalPage = 0 } = this.props.topic?.topics || {}
+    return (
+      <div className={styles.searchContent}>
+        <div className={styles.section}>
+          <SectionTitle title="潮流话题" isShowMore={false}/>
+          <TrendingTopicMore data={pageData} onItemClick={this.onTopicClick}/>
+        </div>
       </div>
+    )
+  }
+  render() {
+    const { currentPage, totalPage } = this.props.topic?.topics || {}
+    return (
+      <List className={styles.searchWrap} noMore={currentPage === totalPage} onRefresh={this.fetchMoreData}>
+        <BaseLayout
+          onSearch={this.onSearch}
+          right={ this.renderRight }
+        >
+          { this.renderContent }
+        </BaseLayout>
+      </List>
     );
   }
 }
-export default IndexPCPage;
+export default withRouter(IndexPCPage);
--- web/layout/topic/topic-detail/pc/index.jsx
+++ web/layout/topic/topic-detail/pc/index.jsx
@@ -1,18 +1,74 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import styles from './index.module.scss';
-
+import { withRouter } from 'next/router';
+import BaseLayout from '@components/base-layout';
+import List from '@components/list'
+import NoData from '@components/no-data';
+import SectionTitle from '@layout/search/h5/components/section-title'
+import DetailsHeader from './components/details-header';
 @inject('site')
 @inject('user')
 @inject('index')
 @observer
 class IndexPCPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      refreshing: false,
+    };
+  }
+  onSearch = (value) => {
+    this.setState({ keyword: value }, () => {
+      this.searchData(value);
+    });
+  }
+   // 右侧 - 活跃用户 版权信息
+   renderRight = () => {
+    // const { pageData = [] } = this.props.search.indexUsers || { pageData: [] };
+    return (
+      <>
+      {
+        // pageData?.length > 0 && (
+          <div className={styles.topicRight}>
+            <div className={styles.section}>
+              <SectionTitle title="活跃用户" onShowMore={this.redirectToSearchResultUser}/>
+              {/* <ActiveUsers data={pageData} onItemClick={this.onUserClick}/> */}
+            </div>
+          </div>
+        // )
+      }
+      </>
+    )
+  }
+  // 中间 -- 话题
+  renderContent = () => {
+    const { pageData = [] } = this.props.topic?.topicDetail || {};
+    return (
+      <div className={styles.topicContent}>
+        <DetailsHeader/>
+        {
+          pageData?.length
+            ? pageData?.map((item, index) => <ThreadContent className={styles.threadContent} data={item} key={index} />)
+            : <NoData />
+        }
+      </div>
+    )
+  }
   render() {
     return (
-      <div className={styles.container}>
-       话题
-      </div>
+      <List className={styles.searchWrap}>
+      <div className={styles.topicWrap}>
+        <BaseLayout
+          onSearch={this.onSearch}
+          right={ this.renderRight }
+        >
+          { this.renderContent }
+        </BaseLayout>
+        </div>
+      </List>
     );
   }
 }
-export default IndexPCPage;
+export default withRouter(IndexPCPage);
