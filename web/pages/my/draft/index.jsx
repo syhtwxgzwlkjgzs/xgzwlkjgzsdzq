a53a733d82178328f99986d384588172b5e3666c
angelzou <angelzou@tencent.com>
2021-05-06 21:47:44 +0800

    feat: 增加我的草稿页面

---
+++ web/pages/my/draft/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import HOCFetchSiteData from '@common/middleware/HOCFetchSiteData';
+import HOCWithLogin from '@common/middleware/HOCWithLogin';
+import { inject, observer } from 'mobx-react';
+import H5Page from '@layout/my/draft/h5';
+import PCPage from '@layout/my/draft/pc';
+
+@inject('site')
+@observer
+class Draft extends React.Component {
+  render() {
+    const { site } = this.props;
+    const { platform } = site;
+
+    if (platform === 'pc') {
+      return <PCPage />;
+    }
+    return <H5Page />;
+  }
+}
+
+// eslint-disable-next-line new-cap
+export default HOCFetchSiteData(HOCWithLogin(Draft));
