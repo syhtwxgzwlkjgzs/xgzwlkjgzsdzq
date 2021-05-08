0ac73e2b47cb0ab058a23b0c6c6cca312eac9362
yzh <810593582@qq.com>
2021-05-08 16:28:40 +0800

    '完成他人页面头部显示以及部分按钮点击事件'

--- web/components/user-center-head/h5/index.jsx
+++ web/components/user-center-head/h5/index.jsx
@@ -4,6 +4,29 @@ import Avatar from '@components/avatar';
 import { Button, Icon } from '@discuzq/design';
 
 export default class index extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isShield: false, // 表示是否屏蔽
+      isAttention: false, // 表示是否关注
+    }
+  }
+
+  // 点击屏蔽
+  handleChangeShield = () => {
+    this.setState({
+      isShield: !this.state.isShield
+    })
+  }
+
+  // 点击关注
+  handleChangeAttention = () => {
+    this.setState({
+      isAttention: !this.state.isAttention
+    })
+  }
+
   render() {
     return (
       <div className={styles.h5box}>
@@ -38,14 +61,38 @@ export default class index extends Component {
         </div>
         {/* 下 */}
         <div className={styles.userBtn}>
-          <Button type="primary">
+          {/* <Button type="primary">
             <Icon name="CompileOutlined" />
             <span className={styles.userBtnText}>编辑资料</span>
           </Button>
           <Button>
             <Icon name="PoweroffOutlined" />
             <span className={styles.userBtnText}>退出登录</span>
+          </Button> */}
+          <Button onClick={this.handleChangeAttention} type="primary">
+            {
+              this.state.isAttention ? (
+                <>
+                  <Icon name="CheckOutlined" />
+                  <span className={styles.userBtnText}>已关注</span>
+                </>
+              ) : (
+                <>
+                  <Icon name="PlusOutlined" />
+                  <span className={styles.userBtnText}>关注</span>
+                </>
+              )
+            }
           </Button>
+          <Button>
+            <Icon name="NewsOutlined" />
+            <span className={styles.userBtnText}>发私信</span>
+          </Button>
+        </div>
+        {/* 右上角屏蔽按钮 */}
+        <div onClick={this.handleChangeShield} className={styles.shieldBtn}>
+          <Icon name="ShieldOutlined" />
+          <span>{this.state.isShield ? '解除屏蔽' : '屏蔽'}</span>
         </div>
       </div>
     )
