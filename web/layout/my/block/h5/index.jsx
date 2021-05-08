a6839677421eeeadfb4b1126d3607e5aca2c3fc0
lijunjun <li13754950174@163.com>
2021-05-08 11:25:36 +0800

    feat: h5屏蔽页面

--- web/layout/my/block/h5/index.jsx
+++ web/layout/my/block/h5/index.jsx
@@ -12,10 +12,13 @@ import Avatar from '@components/avatar';
 @inject('search')
 @observer
 class Index extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    console.log(this.props, '数据');
-    const data = [1,2,3,4];
-    const url = 'https://discuz-service-test-1258344699.cos.ap-guangzhou.myqcloud.com/public/attachments/2021/04/15/JLaJTCQwFQMCUZ0qOeyjke9LXcf1zuQEjwTiI8Ul.jpg?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDCJAnwjKjthEk6HBm6fwzhCLFRRBlsBxG%26q-sign-time%3D1620378001%3B1620464461%26q-key-time%3D1620378001%3B1620464461%26q-header-list%3Dhost%26q-url-param-list%3D%26q-signature%3D1095d4caaf7c39f102449f8aaaf4c308d43f8266&x-cos-security-token=abc&imageMogr2/thumbnail/500x500';
+    const { search } = this.props;
+    const { pageData = [], currentPage, totalPage } = search.users;
+   
     return (
       <div className={styles.shieldBox}>
         <Header />
@@ -23,24 +26,24 @@ class Index extends React.Component {
           共有2位用户
         </div>
         {
-          data?.length
+          pageData?.length
             ? (
               <List
                 className={styles.list}
-                noMore={true}
+                noMore={currentPage >= totalPage}
               >
                 {
-                  data.map((item, index) => (
+                  pageData.map((item, index) => (
                     <div className={styles.haieldImg} key={index}>
                       <div className={styles.haieldImgBox}>
                         <div className={styles.haieldImgHead}>
                           <Avatar 
                             className={styles.img}
-                            image={url}
-                            name={'名字'}
-                            userId={1}
+                            image={item.avatar}
+                            name={item.nickname}
+                            userId={item.userId}
                           />
-                          <p className={styles.haieldName}>名字</p>
+                          <p className={styles.haieldName}>{item.nickname}</p>
                         </div>
                         <div className={styles.haieldName}>
                           <Button className={styles.haieldButton}>解除屏蔽</Button>
