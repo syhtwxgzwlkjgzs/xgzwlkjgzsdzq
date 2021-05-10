03c37aacb82ac92bf201e464cf2abccac7228dff
sun-conghua <1335411996@qq.com>
2021-05-10 20:47:04 +0800

    H5草稿箱

--- web/components/my-draft/index.js
+++ web/components/my-draft/index.js
@@ -1,61 +1,57 @@
 /**
  * 发帖页标题
- * @prop {string} title 输入标题值
- * @prop {string} placeholder
- * @prop {boolean} isDisplay 是否显示标题
- * @prop {function} onChange change事件，输出当前标题值
+ * @prop {function} deleteDraft 删除草稿事件
+ * @prop {function} clickDraftTxt 点击进入编辑草稿事件
+ * @prop {function} onRefresh 触底事件，需要传一个promise
+ * 侧滑删除组件详情参考 https://github.com/sandstreamdev/react-swipeable-list
  */
 import React, { memo, useState, useEffect, useRef } from 'react';
-import Header from '@components/header';
+import { inject, observer } from 'mobx-react';
 import { SwipeableList, SwipeableListItem } from '@sandstreamdev/react-swipeable-list';
 import '@sandstreamdev/react-swipeable-list/dist/styles.css';
-import List from '@components/list';
-import { PullToRefresh } from '@discuzq/design';
+import List from '@components/base-layout';
+import ThreadCenterView from '@components/thread/ThreadCenterView';
 import styles from './index.module.scss';
 
 import PropTypes from 'prop-types';
 
-const MyDrafts = ({pc, ...props }) => {
-    // state 标题值
-    const listRef = useRef(null);
-    const [list, setList] = useState([{ id: 1, title: '运行dzq只适合带宽按量付的云服务器', span: '编辑于 2分钟前' }, { id: 2, title: 'Users你好，你的注册申请已审核通过', span: '编辑于 2021-3-19' }, { id: 3, title: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', span: '编辑于 2021-3-16' }, { id: 4, title: '运行dzq只适合带宽按量付的云服务器', span: '编辑于 2分钟前' }, { id: 5, title: '运行dzq只适合带宽按量付的云服务器', span: '编辑于 2分钟前' }, { id: 6, title: '运行dzq只适合带宽按量付的云服务器', span: '编辑于 2分钟前' }, { id: 7, title: '运行dzq只适合带宽按量付的云服务器', span: '编辑于 2分钟前' }, { id: 8, title: '运行dzq只适合带宽按量付的云服务器', span: '编辑于 2分钟前' }, { id: 9, title: '运行dzq只适合带宽按量付的云服务器', span: '编辑于 2分钟前' },]);
+const MyDrafts = ({ pc, index, deleteDraft, clickDraftTxt, onRefresh, ...props }) => {
+    const [list, setList] = useState([]);
 
-    const [refresh, setRefresh] = useState(true);
-    const onRefresh = () => {
-        setRefresh(false);
-    };
+    useEffect(async () => {
+        await index.getReadThreadList(); // 先用帖子数据模拟
+        setList(index.threads.pageData)
+    }, []);
 
-    const deleteDraft = (e) => { // 删除草稿事件
+    const deleteDrafts = (e) => { // 删除草稿事件
+        deleteDraft(e)
         console.log('删除了', e)
     }
-    const clickDraftTxt = (e) => {
+    const clickDraftTxts = (e) => {
+        clickDraftTxt(e)
         console.log(`点击进入`, e)
     }
-    const onScroll = () => {
-        const el = listRef.current.offsetTop;
-        console.log('滚动了', el);
+    const onRefreshs = () => {
+        onRefresh()
+        console.log('触底了')
     }
 
     return (
         <div className={styles.drafts_box}>
-            {/* 头部 */}
-            <Header isBackCustom={() => { this.props.handleSetState({ draftShow: true }); return false; }} />
-            {/* 头部结束 */}
             {/* 草稿箱列表部分 */}
-            <List className={styles.list} onScroll={onScroll} allowRefresh={false} onRefresh={() => { console.log('触底了'); }}>
+            <List className={styles.list} onRefresh={() => { onRefreshs() }} onPullDown={() => { console.log('下拉了') }} showPullDown={true}>
                 {/* 显示草稿条数 */}
                 <div className={styles.drafts_lenth}><span>{list.length} 条草稿</span></div>
                 {/* 显示草稿条数结束 */}
-
-                <div className={styles.drafts_listbox} ref={listRef}>
+                <div className={styles.drafts_listbox} >
                     {list.map((item, index) => {
                         return <SwipeableListItem swipeLeft={{
                             content: <div className={styles.drafts_delete}>删除</div>,
-                            action: () => { deleteDraft(item.id) }
+                            action: () => { deleteDrafts(item.postId) }
                         }} key={index}>
-                            <div className={styles.drafts_list} onClick={() => clickDraftTxt(item.id)}>
-                                <p className={styles.drafts_p}>{item.title}</p>
-                                <p className={styles.drafts_time}>{item.span}</p>
+                            <div className={styles.drafts_list} onClick={() => clickDraftTxts(item.postId)}>
+                                <ThreadCenterView data={item} />
+                                <p className={styles.drafts_time}>{item.diffTime}</p>
                             </div>
                         </SwipeableListItem>
                     })}
@@ -67,22 +63,16 @@ const MyDrafts = ({pc, ...props }) => {
 };
 
 MyDrafts.propTypes = {
-    // title: PropTypes.string,
-    // isDisplay: PropTypes.bool,
-    // placeholder: PropTypes.string,
-    // onChange: PropTypes.func,
-    // onFocus: PropTypes.func,
-    // onBlur: PropTypes.func,
+    deleteDraft: PropTypes.func,
+    clickDraftTxt: PropTypes.func,
+    onRefresh: PropTypes.func,
 };
 
 // 设置props默认类型
 MyDrafts.defaultProps = {
-    // title: '',
-    // isDisplay: false,
-    // placeholder: '标题(可选)',
-    // onChange: () => { },
-    // onFocus: () => { },
-    // onBlur: () => { },
+    deleteDraft: () => { },
+    clickDraftTxt: () => { },
+    onRefresh: () => { }, // 触底需要传一个pormise
 };
 
-export default memo(MyDrafts);
+export default inject('index')(observer(memo(MyDrafts)));
