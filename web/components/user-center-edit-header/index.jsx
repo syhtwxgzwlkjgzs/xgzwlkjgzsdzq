e7eec0e569ffbaf77da845a485833237186e25ae
yzh <810593582@qq.com>
2021-05-10 11:18:14 +0800

    '组件拆分'

--- web/components/user-center-edit-header/index.jsx
+++ web/components/user-center-edit-header/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles from './index.module.scss';
 import Avatar from '@components/avatar';
 import UserCenterHeaderImage from '@components/user-center-header-images';
-import { Button, Icon } from '@discuzq/design';
+import { Icon } from '@discuzq/design';
 
 export default class index extends Component {
   render() {
@@ -17,58 +17,6 @@ export default class index extends Component {
           <div className={styles.userCenterEditDec}>
             <Icon name="CompileOutlined" />
             <span className={styles.text}>不会开飞机的程序员，不是一个好的摄影师不会开飞机的程序员，不是一个好的摄影师不会开飞机的程序员，不是一个好的摄影师不会开飞机的程序员，不是一个好的摄影师</span>
-          </div>
-        </div>
-        {/* middle */}
-        <div className={styles.userCenterEditMiddle}>
-          <h3>个人信息</h3>
-          <div className={styles.userCenterEditLabel}>
-            <label>昵称</label>
-            <div>Users</div>
-          </div>
-          <div className={styles.userCenterEditLabel}>
-            <label>用户名</label>
-            <div>Users</div>
-          </div>
-          <div className={styles.userCenterEditItem}>
-            <div className={styles.userCenterEditLabel}>
-              <label>手机号码</label>
-              <div>182****420</div>
-            </div>
-            <div><Icon name="RightOutlined" /></div>
-          </div>
-          <div className={styles.userCenterEditItem}>
-            <div className={styles.userCenterEditLabel}>
-              <label>账户密码</label>
-              <div>修改</div>
-            </div>
-            <div><Icon name="RightOutlined" /></div>
-          </div>
-          <div className={styles.userCenterEditItem}>
-            <div className={styles.userCenterEditLabel}>
-              <label>支付密码</label>
-              <div>修改</div>
-            </div>
-            <div><Icon name="RightOutlined" /></div>
-          </div>
-          <div className={styles.userCenterEditLabel} style={{border:'none'}}>
-            <label>微信</label>
-            <div><img /> Users（解绑）</div>
-          </div>
-        </div>
-        {/* bottom */}
-        <div className={styles.userCenterEditBottom}>
-          <h3>实名认证</h3>
-          <div className={styles.userCenterEditItem}>
-            <div className={styles.userCenterEditLabel}>
-              <label>申请实名认证</label>
-              <div>去认证</div>
-            </div>
-            <div><Icon name="RightOutlined" /></div>
-          </div>
-          <div className={styles.userCenterEditBtn}>
-            <Button>取消</Button>
-            <Button type="primary">保存</Button>
           </div>
         </div>
       </>
