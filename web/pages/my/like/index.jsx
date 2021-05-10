3427c6756dd3594f8754982ab02707e5e1eb89fd
lixiaoxia <2416388215@qq.com>
2021-05-10 10:18:42 +0800

    fix: 屏蔽

--- web/pages/my/like/index.jsx
+++ web/pages/my/like/index.jsx
@@ -2,35 +2,45 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import IndexH5Page from '@layout/my/like/h5';
 import IndexPCPage from '@layout/my/like/pc';
-import { readThreadList } from '@server';
+import { readThreadList, readTopicsList } from '@server';
 import HOCFetchSiteData from '@common/middleware/HOCFetchSiteData';
 
 @inject('site')
 @inject('index')
+@inject('search')
 @observer
 class Index extends React.Component {
   page = 1;
   prePage = 10;
   static async getInitialProps(ctx) {
+    const result = await readTopicsList();
     const threads = await readThreadList({ params: { filter: {}, sequence: 0, perPage: 10, page: 1 } }, ctx);
     return {
       serverIndex: {
         threads: threads && threads.code === 0 ? threads.data : null,
+      },
+      serverSearch: {
+        topics: result?.data
       },
     };
   }
 
   constructor(props) {
     super(props);
-    const { serverIndex, index } = this.props;
+    const { serverIndex, index, serverSearch, search } = this.props;
     serverIndex && serverIndex.threads && index.setThreads(serverIndex.threads);
+    serverSearch && serverSearch.topics && search.setTopics(serverSearch.topics);
   }
 
   componentDidMount() {
-    const { index } = this.props;
+    const { index, search } = this.props;
     const hasThreadsData = !!index.threads;
+    const hasTopics = !!search.topics;
     if (!hasThreadsData) {
       this.props.index.getReadThreadList();
+    }
+    if(!hasTopics){
+      search.getTopicsList();
     }
   }
   dispatch = async (type, data = {}) => {
