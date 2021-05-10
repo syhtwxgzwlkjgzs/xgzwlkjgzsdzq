d1b7e56690cdbf86d60b1fee58c730000e297cbf
Sean Luo <luoxinran@eims.com.cn>
2021-05-10 20:27:14 +0800

    Copy IM component from h5 page to mini

--- mini/src/layout/message/index.jsx
+++ mini/src/layout/message/index.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
-import styles from './index.module.scss';
+// import React from 'react';
+// import styles from './index.module.scss';
+// import { inject, observer } from 'mobx-react';
+// import { Button } from '@discuzq/design';
+// import { View } from '@tarojs/components';
+
+// const Index = inject('site')(
+//   observer(() => {
+//     const test = () => {};
+
+//     return (
+//       <View className={styles.container}>
+//         <Button onClick={test}>mini test</Button>
+//       </View>
+//     );
+//   }),
+// );
+
+// export default Index;
+
+import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { Button } from '@discuzq/design';
-import { View } from '@tarojs/components';
+import InstantMessaging from '../../components/message/instant-messaging';
 
-const Index = inject('site')(observer(() => {
-  const test = () => {};
+@inject('site')
+@observer
+class MessagePage extends Component {
+  state = {
+    messagesHistory: [],
+  };
 
-  return (
-    <View className={styles.container}>
-      <Button onClick={test}>mini test</Button>
-    </View>
-  );
-}));
+  doSubmit = (val) => {
+    if (!val) return;
+    const { messagesHistory } = this.state;
+    this.setState({ messagesHistory: [...messagesHistory, val] });
+    return true;
+  };
 
-export default Index;
+  render() {
+    const { messagesHistory } = this.state;
+    return <InstantMessaging messagesHistory={messagesHistory} onSubmit={this.doSubmit} persona={'myself'} />;
+  }
+}
+
+export default MessagePage;
