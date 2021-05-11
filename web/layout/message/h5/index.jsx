4d28b587b487b941c0f0c5b8aa8c56660cc1664c
Sean Luo <luoxinran@eims.com.cn>
2021-05-11 10:53:55 +0800

    Move IM component into web/components/message folder

--- web/layout/message/h5/index.jsx
+++ web/layout/message/h5/index.jsx
@@ -1,7 +1,7 @@
 import React, { memo, useState, useEffect } from 'react';
 import styles from './index.module.scss';
 import { inject, observer } from 'mobx-react';
-import InstantMessaging from '../instant-messaging';
+import InstantMessaging from '../../../components/message/instant-messaging';
 
 import NoticeItem from '@components/message/notice-item';
 import SliderLeft from '@components/message/slider-left';
@@ -16,13 +16,13 @@ const Index = ({ page, subPage, dialogId }) => {
   // hooks
   useEffect(() => {
     setList(mock[type]); // 设置渲染数据
-  }, [])
+  }, []);
 
   // handle
   const handleDelete = (id) => {
-    const _list = [...list].filter(item => item.id !== id);
+    const _list = [...list].filter((item) => item.id !== id);
     setList(_list);
-  }
+  };
 
   const doSubmit = (val) => {
     if (!val) return;
@@ -32,13 +32,7 @@ const Index = ({ page, subPage, dialogId }) => {
 
   return (
     <div className={styles.container}>
-      <SliderLeft
-        list={list}
-        offsetLeft={'-74px'}
-        type={type}
-        RenderItem={NoticeItem}
-        onDelete={handleDelete}
-      />
+      <SliderLeft list={list} offsetLeft={'-74px'} type={type} RenderItem={NoticeItem} onDelete={handleDelete} />
       <InstantMessaging messagesHistory={messagesHistory} onSubmit={doSubmit} persona={'myself'} />
     </div>
   );
