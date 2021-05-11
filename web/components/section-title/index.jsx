ddc92a6bd092371380dc7f05cd3c626391dae071
lixiaoxia <2416388215@qq.com>
2021-05-11 20:01:13 +0800

    fix: PC栏目头部去掉margin

--- web/components/section-title/index.jsx
+++ web/components/section-title/index.jsx
@@ -12,11 +12,11 @@ import styles from './index.module.scss';
  * @prop {boolean} isShowMore 是否显示更多
  * @prop {boolean} rightText 右侧描述文字
  */
-const Index = ({ icon = { type: '' }, title, leftNum, onShowMore, isShowMore = true, rightText }) => (
+const Index = ({ icon = {}, title, leftNum, onShowMore, isShowMore = true, rightText }) => (
   <div className={styles.container}>
     <div className={styles.left}>
-      <Icon className={`${styles[`icon${icon.type}`]} ${styles.margin}`} name={icon.name} size={16} color={icon.color}/>
-      <div className={styles.title}>{title}</div>
+      <Icon className={styles[`icon${icon.type}`]} name={icon.name} size={16} color={icon.color}/>
+      <div className={`${styles.title} ${JSON.stringify(icon) === '{}' ? styles.noMargin : ''}`}>{title}</div>
       <div className={styles.num}>{leftNum}</div>
     </div>
     <div className={styles.right}>
