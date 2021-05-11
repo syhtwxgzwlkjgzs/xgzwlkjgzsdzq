7417cc327411129d1d942d23228298b9f13a6738
joneeeeli <joneeeeli@tencent.com>
2021-05-11 10:23:41 +0800

    命名更改

--- web/pages/message/index.jsx
+++ web/pages/message/index.jsx
@@ -16,7 +16,7 @@ const Index = ({ site }) => {
    * 从url query参数中取值page、subPage、dialogId
    * page=index: 消息首页
    * page=thread: 帖子通知，subPage=at/reply/like为贴子通知下@我的、回复我的、点赞我的3个子页面
-   * page=finance: 财务通知
+   * page=financial: 财务通知
    * page=account: 账号消息
    * page=chat: 聊天对话，dialogId=xxx为当前对话id
    *
@@ -24,7 +24,7 @@ const Index = ({ site }) => {
   const router = useRouter();
   // 参数过滤
   const params = (({ page, subPage, dialogId }) => {
-    if (!['index', 'thread', 'finance', 'account', 'chat'].includes(page)) {
+    if (!['index', 'thread', 'financial', 'account', 'chat'].includes(page)) {
       page = 'index';
     }
 
