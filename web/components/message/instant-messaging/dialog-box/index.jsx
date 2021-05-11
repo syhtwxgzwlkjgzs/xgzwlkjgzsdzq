873dff81bebb7b5c9c10737136bff11d782fc1a9
Sean Luo <luoxinran@eims.com.cn>
2021-05-11 11:22:00 +0800

    Replace format date function in IM component with @common/utils/diff-date

--- web/components/message/instant-messaging/dialog-box/index.jsx
+++ web/components/message/instant-messaging/dialog-box/index.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import { Icon } from '@discuzq/design';
+import { diffDate } from '@common/utils/diff-date';
 
 import styles from './index.module.scss';
 
 const DialogBox = (props) => {
   const { shownMessages, persona = 'itself', dialogBoxRef, platform } = props;
 
-  const formatMsgDate = (timestamp) => {
-    const date = new Date(timestamp),
-      m = date.getMonth() + 1,
-      d = date.getDate();
-    return (m < 10 ? '0' + m : m) + '-' + (d < 10 ? '0' + d : d) + ' ' + date.toTimeString().substr(0, 5);
-  };
-
-  const displayMsgTime = (curTimestamp) => {
-    const formattedDate = formatMsgDate(curTimestamp);
-    return <div className={styles.msgTime}>{formattedDate}</div>;
-  };
-
   return (
     <div className={platform === 'pc' ? styles.pcDialogBox : styles.h5DialogBox} ref={dialogBoxRef}>
       {shownMessages.map(({ timestamp, displayTimePanel, text }, idx) => (
         <React.Fragment key={idx}>
-          {displayTimePanel && displayMsgTime(timestamp)}
+          {displayTimePanel && <div className={styles.msgTime}>{diffDate(timestamp)}</div>}
           <div className={(persona === 'myself' ? `${styles.myself}` : `${styles.itself}`) + ` ${styles.persona}`}>
             <div className={styles.profileIcon}>
               <Icon name="UserOutlined" size={20} color={'var(--color-primary)'} />
