eb504af97bcf1f67e20cb58cfba0812c8e29f4cb
Sean Luo <luoxinran@eims.com.cn>
2021-05-12 21:21:35 +0800

    Add financial index component

--- web/components/message/message-financial/index.jsx
+++ web/components/message/message-financial/index.jsx
@@ -1,26 +1,39 @@
-import React, { memo, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { inject, observer } from 'mobx-react';
-import { PullDownRefresh } from '@discuzq/design';
 
 import Notice from '@components/message/notice';
 
 import styles from './index.module.scss';
 
 const FinancialIndex = ({ message }) => {
-  const { readDialogMsgList, dialogMsgList, createDialogMsg } = message;
+  const { readFinancialMsgList } = message;
+  const [financialList, setFinancialList] = useState([]);
 
-  useEffect(() => {
-    console.log(message);
+  useEffect(async () => {
+    await readFinancialMsgList();
+    setFinancialList([...message.financialMsgList.list]);
   }, []);
 
-  const handleDelete = () => {
-    return true;
+  const handleDelete = (item) => {
+    message.deleteMsg(item.id, 'financialMsgList');
   };
+
+  const formatFinancialList = (list) => {
+    const newList = [];
+    list.forEach(({ amount, content, createdAt, id, threadId, type, userAvatar, userId, username }) => {
+      const newItem = { amount, content, createdAt, id, threadId, type, userAvatar, userId, userName: username };
+      newList.push(newItem);
+    });
+
+    return newList;
+  };
+
+  // console.log(formatFinancialList(financialList));
+  console.log(message);
 
   return (
     <div className={styles.wrapper}>
-      <div>this is financial</div>
-      <Notice list={[]} type={'financial'} onBtnClick={handleDelete} />
+      <Notice list={formatFinancialList(financialList)} type={'financial'} onBtnClick={handleDelete} />
     </div>
   );
 };
