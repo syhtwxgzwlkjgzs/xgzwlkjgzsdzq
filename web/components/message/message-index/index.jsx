f03289c06210c8a6b5d220a9c0d67c50eb938021
Sean Luo <luoxinran@eims.com.cn>
2021-05-12 14:51:02 +0800

    Added pull up refresh list

--- web/components/message/message-index/index.jsx
+++ web/components/message/message-index/index.jsx
@@ -43,6 +43,7 @@ export class MessageIndex extends Component {
   handleRefresh = () => {
     const { message } = this.props;
     this.setState({ finished: false });
+    console.log('Triggering refreshing at TOP!');
     setTimeout(async () => {
       await message.readDialogList(message.dialogList.currentPage);
       this.setState({ finished: true });
