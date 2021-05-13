fb1c7eeaf0c45bcf92426d9ee8f384a59eca34af
sun-conghua <1335411996@qq.com>
2021-05-13 20:21:16 +0800

    小程序草稿箱接入数据

--- mini/src/subPages/my/draft/index.jsx
+++ mini/src/subPages/my/draft/index.jsx
@@ -1,37 +1,32 @@
 import React, { Component } from 'react';
 import { View } from '@tarojs/components';
+import Taro from '@tarojs/taro';
 import Page from '@components/page';
-import styles from './index.module.scss';
+import { Toast } from '@discuzq/design';
+import List from '@components/list';
 import { inject, observer } from 'mobx-react';
 import SliderLeft from '@components/slider-left';
-import List from '@components/list';
+import { THREAD_LIST_FILTER_COMPLEX } from '@common/constants/index';
+import { diffDate } from '@common/utils/diff-date';
 
-/**
-* 小程序草稿箱组件
-* @prop {function} onEdit    点击草稿条栏事件
-* @prop {function} dispatch 下拉刷新事件
-* @prop {function} onDelete 删除事件
-* @prop other List Props // List组件所有的属性
-* @example 
-*     <Page
-      dispatch={this.fetchData}
-      onEdit={item => this.handleEdit(item)}
-      onDelete={item => this.handleDelete(item)}
-    />
-*/
+import styles from './index.module.scss';
 // TOOD 目前页面使用的数据展示待组件编辑优化
-class DraftItem extends Component { // 草稿箱渲染部分
-  clickDraftTxts = (item) => { // 点击进入草稿箱
-    this.props.onEdit(item);
-  }
+class DraftItem extends React.Component { // 草稿箱渲染部分
+
+  handleEdit = (item) => {
+    Taro.navigateTo({ url: `/subPages/thread/post/index?id=${item.threadId}` })
+  } // 点击进入草稿箱
+
   render() {
     const { item, listlength, index } = this.props;
     return (
       <View className={styles['drafts-listbox']} >
-        <View className={index === listlength - 1 ? styles['drafts-list-finally'] : styles['drafts-list']}
-          onClick={this.clickDraftTxts.bind(this, item.id)} >
-          <View>{item.text}</View>
-          <View className={styles['drafts-time']}>{item.time}</View>
+        <View
+          className={index === listlength - 1 ? styles['drafts-list-finally'] : styles['drafts-list']}
+          onClick={this.handleEdit}
+        >
+          <View dangerouslySetInnerHTML={{ __html: item.content.text }} />
+          <View className={styles['drafts-time']}>{diffDate(item.createdAt)}</View>
         </View>
       </View>
     )
@@ -39,42 +34,47 @@ class DraftItem extends Component { // 草稿箱渲染部分
 }
 
 @inject('index')
+@inject('thread')
 @observer
 class Index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [
-        { id: 1, text: '运行dzq只适合带宽按量付的云服务器', time: '编辑于 2分钟前' },
-        { id: 2, text: 'Users你好，你的注册申请已审核通过。', time: '编辑于 2021-3-19' },
-        { id: 3, text: '心有猛虎，细嗅蔷薇', time: '编辑于 1分钟前' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-        { id: 4, text: 'pc+h5问答类型 #主题# ，就是暂时不支持二级分类，等待官方升级，欢迎体验。', time: '编辑于 2021-3-16' },
-      ],
-    }
+  page = 1;
+  perPage = 10;
+  componentDidMount() {
+    this.fetchData();
   }
 
-  deleteDraftTxts = (item, index) => { //删除草稿事件
-    this.props.onDelete(item);
+  fetchData = async (isMore) => {
+    const { index } = this.props;
+    if (isMore) this.page += 1;
+    else this.page = 1;
+    await index.getReadThreadList({
+      page: this.page,
+      perPage: this.perPage,
+      filter: { complex: THREAD_LIST_FILTER_COMPLEX.draft },
+    });
+    return;
+  }
+
+  handleDelete = async (item) => { // 删除草稿事件
+    const { thread, index } = this.props;
+    // this.toastInstance = Toast.loading({
+    //   content: '删除中...',
+    //   duration: 0,
+    // });
+    let time = 0;
+    Toast.loading({
+      content: '删除中...',
+      duration: time,
+    });
+    const res = await thread.delete(item.threadId);
+    // this.toastInstance?.destroy();
+    time = 1
+    if (res.code === 0) {
+      const data = (index.threads?.postData || []).filter(elem => elem.threadId !== item.threadId);
+      index.setThreads(data);
+    } else {
+      Toast.error({ content: res.msg });
+    }
   }
 
   render() {
@@ -84,14 +84,14 @@ class Index extends Component {
     return (
       <Page>
         <View className={styles['drafts-box']}>
-          <List onRefresh={() => this.props.dispatch(true)} className={styles.list} noMore={currentPage >= totalPage}>
-            <View className={styles['drafts-lenth']}>{this.state.list.length} 条草稿</View>
+          <List onRefresh={() => this.fetchData(true)} className={styles.list} noMore={currentPage >= totalPage}>
+            <View className={styles['drafts-lenth']}>{list.length} 条草稿</View>
             <SliderLeft
               data={data}
-              list={this.state.list}
+              list={list}
               RenderItem={DraftItem}
-              listlength={this.state.list.length}
-              onBtnClick={this.deleteDraftTxts}
+              listlength={list.length}
+              onBtnClick={item => this.handleDelete(item)}
             />
           </List>
         </View >
