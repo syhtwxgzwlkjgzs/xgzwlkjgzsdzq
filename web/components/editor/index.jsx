167edfb0f25fa9e7b462f91438158b813e287594
angelzou <angelzou@tencent.com>
2021-05-13 21:16:35 +0800

    fix: 编辑器选中才显示加粗工具栏

--- web/components/editor/index.jsx
+++ web/components/editor/index.jsx
@@ -91,7 +91,6 @@ export default function DVditor(props) {
         // 编辑器初始化值
         value,
         focus: () => {
-          setIsFocus(true);
           onFocus();
         },
         // input: () => {
@@ -107,7 +106,9 @@ export default function DVditor(props) {
           }, 100);
         },
         // 编辑器中选中文字后触发，PC才有效果
-        select: () => {},
+        select: (value) => {
+          if (value) setIsFocus(true);
+        },
         counter: {
           enable: true,
           after(count) {
@@ -119,7 +120,7 @@ export default function DVditor(props) {
         toolbarConfig: {
           hide: !!pc,
           pin: true,
-          bubbleHide: !pc,
+          bubbleHide: false,
         },
         bubbleToolbar: pc ? [...baseToolbar] : [],
       },
@@ -127,7 +128,7 @@ export default function DVditor(props) {
     setVditor(editor);
   }
 
-  const className = pc ? 'dvditor pc' : classNames('dvditor', { 'no-focus': !pc && !isFocus });
+  const className = pc ? 'dvditor pc' : classNames('dvditor h5', { 'no-focus': !pc && !isFocus });
 
   return (
     <>
