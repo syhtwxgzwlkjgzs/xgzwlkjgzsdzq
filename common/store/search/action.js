9786cb21a6f1d40daf6c83490a6f12bfa078cfdc
刘宇琳 <burnellliu>
2021-05-14 12:06:00 +0800

    fix: 用户点赞头像

--- common/store/search/action.js
+++ common/store/search/action.js
@@ -340,7 +340,7 @@ class SearchAction extends SearchStore {
 
             // 添加当前用户到按过赞的用户列表
             data.likeReward.users = data.likeReward.users.length ?
-                                    [...data.likeReward.users, userAdded]:
+                                    [userAdded, ...data.likeReward.users]:
                                     [userAdded];
           } else {
             // 从按过赞用户列表中删除当前用户
