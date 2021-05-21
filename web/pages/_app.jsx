01badf0083e4525581305cb0367ccc702f008a3e
LLLLLamHo <linhao.4zhong@163com>
2021-05-22 00:43:31 +0800

    test

--- web/pages/_app.jsx
+++ web/pages/_app.jsx
@@ -15,20 +15,31 @@ class DzqApp extends App {
   }
 
   componentDidMount() {
-    // if (process.env.DISCUZ_RUN === 'static') {
-    //   // 当CSR出现末尾是index，会导致不能正确跳转的问题；
-    //   let { pathname } = window.location;
+    if (process.env.DISCUZ_RUN === 'static') {
+      // // 当CSR出现末尾是index，会导致不能正确跳转的问题；
+      let { pathname } = window.location;
 
-    //   if (pathname !== '' || pathname !== '/') {
-    //     const pathnameArr = pathname.split('/');
-    //     if (pathnameArr[pathnameArr.length - 1] === 'index') {
-    //       pathnameArr.pop();
-    //       pathname = pathnameArr.join('/');
-    //     }
-    //   }
-    //   // csr部署时因方便ngixn部署统一指向index.html,所以统一在此重定向一次
-    //   Router.redirect({ url: `${pathname}${window.location.search}` });
-    // }
+      // if (pathname !== '' || pathname !== '/') {
+      //   const pathnameArr = pathname.split('/');
+      //   if (pathnameArr[pathnameArr.length - 1] === 'index') {
+      //     pathnameArr.pop();
+      //     pathname = pathnameArr.join('/');
+      //   }
+      // }
+
+
+      // 处理nginx不能更改，处理动态路由
+      const threadReg = /\/thread\/[0-9]+/ig;
+      const threadCommentReg = /\/thread\/comment\/[0-9]+/ig;
+      const topicReg = /\/topic\/topic-detail\/[0-9]+/ig;
+
+      if ( threadReg.test(pathname) || threadCommentReg.test(pathname) || topicReg.test(pathname)) {
+        Router.redirect({ url: `${pathname}${window.location.search}` });
+      } else {
+        // csr部署时因方便ngixn部署统一指向index.html,所以统一在此重定向一次
+        Router.redirect({ url: `/` });
+      }
+    }
   }
 
   render() {
