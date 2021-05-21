489798ce59b3160c208d574f6a2273aea9d19cde
jf19981111 <18270711420@163.com>
2021-05-21 17:25:15 +0800

    fix: 修复首页侧边遮挡问题

--- web/layout/index/pc/components/navigation/index.jsx
+++ web/layout/index/pc/components/navigation/index.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Menu, Card } from '@discuzq/design';
-import { noop }  from '@components/thread/utils'
+import { noop } from '@components/thread/utils'
 import styles from './index.module.scss';
+import isServer from '../../../../../../common/utils/is-server';
 
 const Index = ({ categories = [], totalThreads = 0, onNavigationClick = noop }) => {
   const [fistIndex, setFistIndex] = useState(-1);
   const [pID, setPID] = useState(-1);
   const [secondIndex, setSecondIndex] = useState(-1);
-  const onClick =(subIndex, index) => {
+  const onClick = (subIndex, index) => {
     let categoryIds = subIndex.split('/')
     let sequence = 0;
     setFistIndex(categoryIds[0]);
@@ -26,6 +27,27 @@ const Index = ({ categories = [], totalThreads = 0, onNavigationClick = noop }) 
 
     onNavigationClick({ categoryIds, sequence })
   }
+  // 监听浏览器窗口变化
+  const getWindowSize = () => {
+    if (!isServer()) {
+      return {
+        innerHeight: !isServer() ? window.innerHeight : null,
+        innerWidth: !isServer() ? window.innerWidth : null,
+      }
+    }
+  };
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  const handleResize = () => {
+    setWindowSize(getWindowSize());
+  };
+  useEffect(() => {
+    if (!isServer()) // 监听
+      window.addEventListener("resize", handleResize);
+    // 销毁
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
 
   const renderMenuTitle = ({ name, threadCount }) => (
     <div>
@@ -42,23 +64,25 @@ const Index = ({ categories = [], totalThreads = 0, onNavigationClick = noop }) 
   );
 
   const CategoriesContent = () => (
-      <Menu defaultOpeneds={[`${fistIndex}`]} defaultSubmenuActives={[`${fistIndex}`]} defaultActives={[`${fistIndex}/${secondIndex}`]}>
-        {
-          categories?.map((item, index) => (item?.children?.length > 0 ? (
-              <Menu.SubMenu key={index} index={`${item.pid}`}  title={renderMenuTitle(item)}>
-                {item.children.map((child, subIndex) => (
-                    <Menu.Item index={`${item.pid}/${child.pid}`} key={subIndex} onClick={onClick}>{renderSubMenuTitle(child)}</Menu.Item>
-                ))}
-              </Menu.SubMenu>
-          ) : (
-              <Menu.Item index={`${item.pid}/-1`} key={index} onClick={onClick}>{renderMenuTitle(item)}</Menu.Item>
-          )))
-        }
-      </Menu>
+    <Menu defaultOpeneds={[`${fistIndex}`]} defaultSubmenuActives={[`${fistIndex}`]} defaultActives={[`${fistIndex}/${secondIndex}`]}>
+      {
+        categories?.map((item, index) => (item?.children?.length > 0 ? (
+          <Menu.SubMenu key={index} index={`${item.pid}`} title={renderMenuTitle(item)}>
+            {item.children.map((child, subIndex) => (
+              <Menu.Item index={`${item.pid}/${child.pid}`} key={subIndex} onClick={onClick}>{renderSubMenuTitle(child)}</Menu.Item>
+            ))}
+          </Menu.SubMenu>
+        ) : (
+          <Menu.Item index={`${item.pid}/-1`} key={index} onClick={onClick}>{renderMenuTitle(item)}</Menu.Item>
+        )))
+      }
+    </Menu>
   );
-
   return (
-    <Card className={styles.container} style={{ background: '#fff' }} bordered={false}>
+    <Card className={`${styles.container} ${styles.verticalScrollbar}`} style={{
+      background: '#fff', overflowY: 'auto',
+      maxHeight: windowSize?.innerHeight - 80
+    }} bordered={false}>
       <CategoriesContent />
     </Card>
   );
