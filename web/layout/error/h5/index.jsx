2517724d76488a4e526f7a3a9f825bd9f68fe881
kevinchou <kevinchou@tencent.com>
2021-05-21 14:40:39 +0800

    fix: 增加服务器异常页面

--- web/layout/error/h5/index.jsx
+++ web/layout/error/h5/index.jsx
@@ -1,53 +1,27 @@
-// import React from 'react';
-// import { inject, observer } from 'mobx-react';
-// import styles from './index.module.scss';
-
-// @inject('site')
-// @observer
-// class H5CloseSite extends React.Component {
-//   render() {
-//     const { site } = this.props;
-//     const { closeSiteConfig } = site;
-
-//     return (
-//       <div className={styles.page}>
-//         <h1 className={styles.main}>关闭站点</h1>
-//         {closeSiteConfig && <p className={styles.sub}>{closeSiteConfig.detail}</p>}
-//       </div>
-//     );
-//   }
-// }
-
-
-// export default H5CloseSite;
-
-
 import React from 'react';
 import styles from './index.module.scss';
+import {Icon,Button} from '@discuzq/design'
 import Header from '@components/header';
-import {Button} from '@discuzq/design';
-import { inject, observer } from 'mobx-react';
-import Router from '@discuzq/sdk/dist/router';
+import Copyright from '@components/copyright';
 
-@inject('site')
-@observer
-class H5CloseSite extends React.Component {
-  render() {
-    const { site } = this.props;
-    const { closeSiteConfig } = site;
-    return (
-      <div className={styles.page}>
-        <Header/>
-        <img className={styles.img} src='/dzq-img/close.png'/>
-        <h1 className={styles.main}>关闭已站点</h1>
-        {closeSiteConfig && <p className={styles.sub}>{closeSiteConfig.detail}</p>}
-        <div className={styles.fixedBox}>
-          <Button onClick={() => {Router.push({url: '/user/login'});}} size='large' className={styles.btn} type='primary'>管理员登录</Button>
-        </div>
-      </div>
-    );
+export default function ErrorPage() {
+
+  const onReflush = ()=>{
+    window.location.reload()
   }
+
+  return (
+    <div className={styles.page}>
+      <Header></Header>
+
+      <div className={styles.body}>
+      <Icon className={styles.icon}></Icon>
+      <span className={styles.text}>服务器错误 SERVER ERROR</span>
+      </div>
+
+      <div className={styles.footer}>
+      <Button onClick={onReflush} className={styles.button} type='primary'>点我刷新</Button>
+      </div>
+    </div>
+  );
 }
-
-
-export default H5CloseSite;
