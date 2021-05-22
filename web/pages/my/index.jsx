36e31f2fe8b82d03a54e9aa426e94712354830a2
jf19981111 <18270711420@163.com>
2021-05-22 15:35:20 +0800

    fix: 修复点击我的没有返回按钮

--- web/pages/my/index.jsx
+++ web/pages/my/index.jsx
@@ -5,7 +5,8 @@ import { inject, observer } from 'mobx-react';
 import MyH5Page from '@layout/my/h5';
 import MyPCPage from '@layout/my/pc';
 import clearLoginStatus from '@common/utils/clear-login-status';
-import {Button} from '@discuzq/design';
+import { Button } from '@discuzq/design';
+import Header from '@components/header';
 @inject('site')
 @observer
 class MyCenter extends React.Component {
@@ -18,8 +19,11 @@ class MyCenter extends React.Component {
     const { platform } = site;
 
     return (
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '30px 0'}}>
-        <Button onClick={() => {this.logout()}} size='large' type='primary'>退出登录</Button>
+      <div>
+        <Header />
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '30px 0' }}>
+          <Button onClick={() => { this.logout() }} size='large' type='primary'>退出登录</Button>
+        </div>
       </div>
     );
 
