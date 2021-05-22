48c4addeb9c29919b90066ff0dc80fe8593b9ff5
Sean Luo <luoxinran@eims.com.cn>
2021-05-22 21:13:43 +0800

    修复了连续进出帖子详情返回首页不断便宜的情况

--- web/components/base-layout/h5/index.js
+++ web/components/base-layout/h5/index.js
@@ -24,8 +24,7 @@ import styles from './index.module.scss';
 const baseLayoutWhiteList = ['home'];
 
 const BaseLayout = (props) => {
-  const { showHeader = true, showTabBar = false, showPullDown = false, children = null, onPullDown, isFinished = true, curr } = props;
-  const { jumpToScrollingPos } = props.baselayout;
+  const { showHeader = true, showTabBar = false, showPullDown = false, children = null, onPullDown, isFinished = true, curr, onScroll, baselayout } = props;
   const [height, setHeight] = useState(600);
 
   const debounce = (fn, wait) => {
@@ -45,11 +44,20 @@ const BaseLayout = (props) => {
     if (pullDownWrapper?.current) {
       setHeight(pullDownWrapper.current.clientHeight)
     }
-
-    if (listRef?.current && jumpToScrollingPos > 0 && baseLayoutWhiteList.indexOf(props.pageName) !== -1) {
-      listRef.current.jumpToScrollTop(jumpToScrollingPos);
+    if (listRef?.current && props.baselayout.jumpToScrollingPos > 0 &&
+        baseLayoutWhiteList.indexOf(props.pageName) !== -1) {
+      listRef.current.jumpToScrollTop(props.baselayout.jumpToScrollingPos);
     }
   }, [])
+
+  const handleScroll = () => {
+    if(!listRef?.current?.currentScrollTop) {
+      onScroll();
+      return;
+    }
+    baselayout.jumpToScrollingPos = listRef.current.currentScrollTop.current;
+    onScroll(listRef.current.currentScrollTop.current);
+  }
 
   return (
     <div className={styles.container}>
@@ -64,7 +72,7 @@ const BaseLayout = (props) => {
               </PullDownRefresh>
             </div>
           ) : (
-            <List {...props} className={styles.list} ref={listRef}>
+            <List {...props} className={styles.list} ref={listRef} onScroll={handleScroll}>
                 {typeof(children) === 'function' ? children({ ...props }) : children}
             </List>
           )
