7e09a3e355fe93069131886b1b405e3f5b8596ed
Lizhen.Cui <927117360@qq.com>
2021-05-22 20:31:48 +0800

    fix: 详情页小程序移植

---
+++ mini/src/layout/thread copy/components/input-popup/index.jsx
@@ -0,0 +1,58 @@
+
+import React, { useEffect, useState } from 'react';
+import { View } from '@tarojs/components';
+
+import { Popup, Textarea, Button } from '@discuzq/design';
+import styles from './index.module.scss';
+
+const InputPop = (props) => {
+  const { visible, onSubmit, initValue, onClose, inputText = '请输入内容' } = props;
+
+  const [value, setValue] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setValue(initValue || '');
+  }, [initValue]);
+
+  const onSubmitClick = async () => {
+    if (typeof onSubmit === 'function') {
+      try {
+        setLoading(true);
+        const success = await onSubmit(value);
+        if (success) {
+          setValue('');
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  return (
+    <Popup
+      position="bottom"
+      visible={visible}
+      onClose={onClose}
+    >
+      <View className={styles.container}>
+        <View className={styles.main}>
+          <Textarea
+            className={styles.input}
+            maxLength={5000}
+            rows={5}
+            showLimit
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder={inputText}>
+          </Textarea>
+        </View>
+        <Button full loading={loading} onClick={onSubmitClick} className={styles.button} type='primary' size='large'>发布</Button>
+      </View>
+    </Popup>
+    );
+};
+
+export default InputPop;
--- mini/src/pages/thread/index.jsx
+++ mini/src/pages/thread/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { getCurrentInstance } from '@tarojs/taro';
+import Taro from '@tarojs/taro';
 import Page from '@components/page';
 import { inject } from 'mobx-react';
 import { ToastProvider } from '@discuzq/design/dist/components/toast/ToastProvider';
@@ -10,7 +11,67 @@ const MemoToastProvider = React.memo(ToastProvider)
 
 @inject('site')
 @inject('thread')
+@inject('user')
 class Detail extends React.Component {
+  static async getInitialProps(ctx) {
+    const id = ctx?.query?.id;
+    const serverThread = {
+      threadData: null,
+      commentList: null,
+      totalCount: 0,
+      authorInfo: null,
+    };
+
+    if (id) {
+      // 获取帖子详情
+      const res = await readThreadDetail({ params: { threadId: Number(id) } });
+      if (res.code === 0) {
+        serverThread.threadData = res.data;
+      }
+
+      // 获取评论列表
+      const commentRes = await readCommentList({
+        params: {
+          filter: {
+            thread: Number(id),
+          },
+          sort: 'createdAt',
+          page: 1,
+          perPage: 5,
+        },
+      });
+
+      if (commentRes.code === 0) {
+        serverThread.commentList = commentRes.data?.pageData || [];
+        serverThread.totalCount = commentRes.data?.totalCount || 0;
+      }
+    }
+    return {
+      serverThread,
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isServerError:false
+    }
+
+    const { thread, serverThread } = this.props;
+
+    // 初始化数据到store中
+    serverThread?.threadData && thread.setThreadData(serverThread.threadData);
+    serverThread?.commentList && thread.setCommentList(serverThread.commentList);
+    serverThread?.totalCount && thread.setTotalCount(serverThread.totalCount);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.router?.query?.id && this.props.router.query.id !== prevProps.router.query.id) {
+      this.props.thread.reset();
+      this.getPageDate(this.props.router.query.id);
+    }
+  }
 
   async componentDidMount() {
 
@@ -23,15 +84,45 @@ class Detail extends React.Component {
     this.props.thread.reset();
 
     if (id && !this.props?.thread?.threadData?.threadId) {
-      if (!this.props?.thread?.threadData) {
-        this.props.thread.fetchThreadDetail(id);
+      this.getPageDate(id);
+    }
+  }
+
+  async getPageDate(id) {
+    if (!this.props?.thread?.threadData) {
+      const res = await this.props.thread.fetchThreadDetail(id);
+
+      if(res.code !== 0) {
+        this.setState({
+          isServerError: true
+        })
+        return
       }
-      if (!this.props?.thread?.commentList) {
-        const params = {
-          id,
-        };
-        this.props.thread.loadCommentList(params);
+
+      // 判断是否审核通过
+      const isApproved = (this.props.thread?.threadData?.isApproved || 0) === 1;
+      if(!isApproved) {
+        const currentUserId = this.props.user?.userInfo?.id; // 当前登录用户
+        const userId = this.props.thread?.threadData?.user?.userId; // 帖子作者
+        // 不是作者自己。跳回首页
+        if(!currentUserId || !userId || currentUserId !== userId) {
+          Taro.redirectTo({
+            url: `pages/index/index`,
+          });
+          return
+        }
       }
+
+      // 获取作者信息
+      const { site } = this.props;
+      const { platform } = site;
+      const userId = this.props.thread?.threadData?.user?.userId;
+    }
+    if (!this.props?.thread?.commentList) {
+      const params = {
+        id,
+      };
+      this.props.thread.loadCommentList(params);
     }
   }
 
