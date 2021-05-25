f5245d10f15da505bf5298082d5063e90320d2f5
LLLLLamHo <linhao.4zhong@163com>
2021-05-25 21:59:50 +0800

    test

--- web/next.config.js
+++ web/next.config.js
@@ -2,5 +2,6 @@ const nextConfig = require('@discuzq/cli/config/next');
 
 module.exports = nextConfig((config) => {
     config.productionBrowserSourceMaps = true;
+    config.crossOrigin = 'anonymous';
     return config;
 });
