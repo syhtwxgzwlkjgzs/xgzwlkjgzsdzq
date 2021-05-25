933c236f8e97606fd5e837964a070628f5bc602b
崔利振 <927117360@qq.com>
2021-05-25 16:11:09 +0800

    fix: 小程序详情修复

--- mini/src/layout/thread/detail/content/index.jsx
+++ mini/src/layout/thread/detail/content/index.jsx
@@ -3,7 +3,7 @@ import { inject, observer } from 'mobx-react';
 import { View, Text } from '@tarojs/components';
 import { Icon, Button } from '@discuzq/design';
 import { parseContentData } from '../../utils';
-// import ImageDisplay from '@components/thread/image-display'; // components中暂时没有
+import ImageDisplay from '@components/thread/image-content';
 import AudioPlay from '@components/thread/audio-play';
 import PostContent from '@components/thread/post-content';
 import ProductItem from '@components/thread/product-item';
@@ -146,14 +146,13 @@ const RenderThreadContent = inject('user')(
             <View style={{ textAlign: 'center' }} onClick={onContentClick}>
               <Button className={styles.payButton} type="primary">
                 <Icon className={styles.payIcon} name="DollarLOutlined" size={20}></Icon>
-                <p>支付{attachmentPrice}元查看附件内容</p>
+                <View>支付{attachmentPrice}元查看附件内容</View>
               </Button>
             </View>
           )}
 
           {/* 图片 */}
-          {/* 暂无该组件 */}
-          {/* {parseContent.IMAGE && <ImageDisplay imgData={parseContent.IMAGE} />} */}
+          {parseContent.IMAGE && <ImageDisplay imgData={parseContent.IMAGE} />}
 
           {/* 视频 */}
           {parseContent.VIDEO && (
