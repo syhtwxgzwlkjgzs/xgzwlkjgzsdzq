5f8d1cd5daaae529d034f20050b95450d96d9119
v_zjxzzheng <v_zjxzzheng@tencent.com>
2021-05-25 20:47:20 +0800

    feat: 添加PC、mini补充信息页

--- web/layout/user/h5/supplementary/index.jsx
+++ web/layout/user/h5/supplementary/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { withRouter } from 'next/router';
+import HomeHeader from '@components/home-header';
+import Header from '@components/header';
 import { Input, Checkbox, Radio, Upload, Button, Icon } from '@discuzq/design';
 import '@discuzq/design/dist/styles/index.scss';
-import layout from './index.module.scss';
+import PcBodyWrap from '../components/pc-body-wrap';
+import h5layout from './index.module.scss';
+import pclayout from './pc.module.scss';
 
 
 @inject('site')
@@ -11,12 +15,11 @@ import layout from './index.module.scss';
 @inject('thread')
 @inject('supplementary')
 @observer
-class SupplementaryH5Page extends React.Component {
-  constructor(props) {
+class SupplementaryPage extends React.Component {
+  constructor(props){
     super(props);
-    this.state = {
-    };
   }
+  
   handleNameChange = (e) => {
     this.props.supplementary.name = e.target.value;
   }
@@ -34,81 +37,127 @@ class SupplementaryH5Page extends React.Component {
   }
 
   render() {
-    const { supplementary } = this.props;
+    const { supplementary, site } = this.props;
+    const { platform } = site;
+    const layout = platform === 'h5' ? h5layout : pclayout;
 
-    return (
-        <div className={layout.content}>
-          {/* 输入框 start */}
+    const InputItem = ({index = 1}) => (
+        <div className={layout.item}>
           <div className={layout.input}>
-            <div className={layout.title}>姓名</div>
-            <Input focus={true} value={supplementary.name} placeholder="请输入姓名" onChange={this.handleNameChange} />
+            <div className={layout.label}>{index}.姓名</div>
+            <Input className={layout.input_value} focus={true} value={supplementary.name} placeholder="请输入姓名" onChange={this.handleNameChange} />
           </div>
-          {/* 输入框 end */}
-          {/* 多行输入框 start */}
-          <div className={layout.textarea}>
-            <div className={layout.title}>注册原因</div>
-            <Input.Textarea
-              autoHeight={true}
-              showLimit={true}
-              maxLength={30}
-              value={supplementary.cause}
-              placeholder="请输入注册原因"
-              rows={3}
-              onChange={this.handleCauseChange}
-            />
-          </div>
-          {/* 多行输入框 end */}
-          {/* 单选 start */}
-          <div className={layout.checkbox}>
-            <div className={layout.title}>性别</div>
-            <Radio.Group value={supplementary.gender} onChange={this.handleGenderChange}>
-              <Radio name="1">男</Radio>
-              <Radio name="2">女</Radio>
-            </Radio.Group>
-          </div>
-          {/* 单选 end */}
-          {/* 多选 start */}
-          <div className={layout.radio}>
-            <div className={layout.title}>爱好</div>
-            <Checkbox.Group value={supplementary.interest} onChange={this.handleInterestChange}>
-              <Checkbox name="1">复选框</Checkbox>
-              <Checkbox name="2">复选框</Checkbox>
-            </Checkbox.Group>
-          </div>
-          {/* 多选 end */}
-          {/* 图片上传 start */}
-          <div className={layout.imgUpload}>
-            <div className={layout.title}>照片</div>
-            <Upload listType='card' multiple={true} customRequest={() => {
+        </div>
+    );
+    const TextareaItem = ({index = 2}) => (
+      <div className={layout.item}>
+        <div className={layout.textarea}>
+          <div className={layout.label}>{index}.注册原因</div>
+          <Input.Textarea
+            autoHeight={true}
+            showLimit={true}
+            maxLength={30}
+            className={layout.textarea_item}
+            value={supplementary.cause}
+            placeholder="请输入注册原因"
+            rows={3}
+            onChange={this.handleCauseChange}
+          />
+        </div>
+      </div>
+    );
+    const CheckboxItem = ({index = 3}) => (
+      <div className={layout.item}>
+        <div className={layout.checkbox}>
+          <div className={layout.label}>{index}.性别</div>
+          <Radio.Group value={supplementary.gender} onChange={this.handleGenderChange}>
+            <Radio name="1">男</Radio>
+            <Radio name="2">女</Radio>
+          </Radio.Group>
+        </div>
+      </div>
+    );
+    const RadioItem = ({index = 4}) => (
+      <div className={layout.item}>
+        <div className={layout.checkbox}>
+          <div className={layout.label}>{index}.爱好</div>
+          <Checkbox.Group value={supplementary.interest} onChange={this.handleInterestChange}>
+            <Checkbox name="1">复选框</Checkbox>
+            <Checkbox name="2">复选框</Checkbox>
+          </Checkbox.Group>
+        </div>
+      </div>
+    );
+    const ImgUploadItem = ({index = 5}) => (
+      <div className={layout.item}>
+        <div className={layout.imgUpload}>
+          <div className={layout.label}>{index}.照片</div>
+          <Upload listType='card' multiple={true} customRequest={() => {
+            console.log('上传');
+          }}>
+            <div className={layout['imgUpload-dom']}>+</div>
+          </Upload>
+        </div>
+      </div>
+    );
+    const AttachmentsUploadItem = ({index = 6}) => (
+      <div className={layout.item}>
+        <div className={layout.attachmentsUpload}>
+          <span className={layout['attachmentsUpload-left']}>{index}.附件</span>
+          <span className={layout['attachmentsUpload-right']}>
+            <Upload listType='text' multiple={true} customRequest={() => {
               console.log('上传');
             }}>
-              <div className={layout['imgUpload-dom']}>+</div>
+            <Icon size='small' name='PaperClipOutlined' />
+            <span>上传附件</span>
             </Upload>
-          </div>
-          {/* 图片上传 end */}
-          {/* 附件上传 start */}
-          <div className={layout.attachmentsUpload}>
-            <span className={layout['attachmentsUpload-left']}>附件</span>
-            <span className={layout['attachmentsUpload-right']}>
-              <Upload listType='text' multiple={true} customRequest={() => {
-                console.log('上传');
-              }}>
-                <Icon size='small' name='LinkOutlined' />
-                <span>上传附件</span>
-              </Upload>
-            </span>
-          </div>
-          {/* 附件上传 ennd */}
-          {/* 提交 start */}
-          <Button className={layout.button} type="primary" onClick={() => {
-            console.log('注册');
-          }}>
-            提交
-          </Button>
-          {/* 提交 end */}
+          </span>
         </div>
+      </div>
+    );
+
+    return (
+      <PcBodyWrap>
+        <div className={layout.container}>
+          {
+            platform === 'h5'
+              ? <HomeHeader hideInfo mode='login'/>
+              : <>
+                  <Header/>
+                  <div className={layout.title}>填写补充信息</div>
+                </>
+          }
+          <div className={layout.content}>
+            {/* 输入框 start */}
+              <InputItem/>
+            {/* 输入框 end */}
+            {/* 多行输入框 start */}
+              <TextareaItem/>
+            {/* 多行输入框 end */}
+            {/* 单选 start */}
+              <CheckboxItem/>
+            {/* 单选 end */}
+            {/* 多选 start */}
+              <RadioItem/>
+            {/* 多选 end */}
+            {/* 图片上传 start */}
+              <ImgUploadItem/>
+            {/* 图片上传 end */}
+            {/* 附件上传 start */}
+              <AttachmentsUploadItem/>
+            {/* 附件上传 ennd */}
+            {/* 提交 start */}
+            <Button className={layout.button} type="primary" onClick={() => {
+              console.log('注册');
+            }}>
+              提交
+            </Button>
+            {/* 提交 end */}
+          </div>
+        </div>
+      </PcBodyWrap>
     );
   }
 }
 
-export default withRouter(SupplementaryH5Page);
+export default withRouter(SupplementaryPage);
