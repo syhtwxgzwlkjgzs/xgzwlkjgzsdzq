d9e99ce59be8bfb7205e4e7ae2ead7856cfbb705
fishcui <fishcui@tencent.com>
2021-05-28 18:44:26 +0800

    feat: 新增腾讯云验证码高阶组件

---
+++ web/middleware/HOCTencentCaptcha.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { inject } from 'mobx-react';
+
+export default function HOCTencentCaptcha(Component) {
+  @inject('site')
+  class HOCTencentCaptchaComponent extends React.Component {
+    captcha = null;
+    constructor(props) {
+      super(props);
+    }
+
+    showCaptcha = () => new Promise((resolve, reject) => {
+      // 验证码实例为空，则创建实例
+      const { webConfig } = this.props.site;
+      const qcloudCaptchaAppId = webConfig?.qcloud?.qcloudCaptchaAppId;
+
+      import('@discuzq/sdk/dist/common_modules/sliding-captcha').then(({
+        TencentCaptcha,
+      }) => {
+        if (!this.captcha) {
+          this.captcha = new TencentCaptcha(qcloudCaptchaAppId, (res) => {
+            if (res.ret === 0) {
+              resolve({
+                captchaRandStr: res.randstr,
+                captchaTicket: res.ticket,
+              });
+            }
+          });
+        }
+        // 显示验证码
+        this.captcha.show();
+      });
+    })
+
+    render() {
+      return <Component {...this.props} showCaptcha={this.showCaptcha} /> ;
+    }
+  }
+
+  return HOCTencentCaptchaComponent;
+}
