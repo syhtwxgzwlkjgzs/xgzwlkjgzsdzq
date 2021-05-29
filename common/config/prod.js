fc2545b194d5e8360d9960b37662d419da423839
LLLLLamHo <linhao.4zhong@163com>
2021-05-29 21:42:28 +0800

    test

--- common/config/prod.js
+++ common/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   TITLE: 'Discuz!Q',
-  COMMOM_BASE_URL: process.env.DISCUZ_ENV === 'web' ? 'https://discuzv3-dev.dnspod.dev' : 'https://discuzv3-dev.dnspod.dev',
+  COMMOM_BASE_URL: process.env.DISCUZ_ENV === 'web' ? '' : 'https://discuzv3-dev.dnspod.dev',
 };
