2dd7a058055e35ef301608a05f5d064fc9830dcb
VS-ZX <2417818740@qq.com>
2021-06-01 20:10:49 +0800

    feat: 同步、完善小程序消息模块

--- mini/src/components/message/message-thread/index.jsx
+++ mini/src/components/message/message-thread/index.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { inject, observer } from 'mobx-react';
 import { View } from '@tarojs/components';
 import styles from './index.module.scss';
 
-const Index = ({message}) => {
+import Notice from '@components/message/notice';
+
+const Index = ({ message }) => {
+  const { threadMsgList, readThreadMsgList, deleteMsg } = message;
+  const { list, currentPage, totalPage } = threadMsgList;
+
+  useEffect(() => {
+    fetchMessageData(1)
+  }, []);
+
+  // 请求数据
+  const fetchMessageData = async (initPage = 0) => {
+    return await readThreadMsgList(initPage || currentPage + 1);
+  }
+
   return (
     <View className={styles.container}>
-      帖子消息
+      <Notice
+        noMore={currentPage >= totalPage}
+        list={list}
+        type='thread'
+        onPullDown={() => fetchMessageData(1)}
+        onScrollBottom={() => fetchMessageData()}
+        onBtnClick={(item) => deleteMsg(item.id, 'threadMsgList')}
+      />
     </View>
   )
 }
