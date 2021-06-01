26f35e1473027bccdd2209c2677daa6d80d911bd
Sean Luo <luoxinran@eims.com.cn>
2021-06-01 23:05:50 +0800

    img算作四行的长度

--- common/utils/fuzzy-calc-content-length.js
+++ common/utils/fuzzy-calc-content-length.js
@@ -2,19 +2,22 @@
 export default function fuzzyCalcContentLength(content, lengthInLine = 50) {
     content = '' + content;
     if (!content || content === '') return;
-    let newContent = content.substr(0, content.lastIndexOf('<img')); // 防止图片被后端字符限制给折断
+
+    const EMOJ_SIZE = 3;
+    const IMG_SIZE = lengthInLine * 4;
+    const countImgs = (content.match(/<img/g) || []).length;
+    const countEmojs = (content.match("qq-emotion") || []).length;
 
     // 检查表情，表情暂时计算为3个字符
-    newContent = newContent.replace(/<img.*?class="(.*?)(qq-emotion)(.*?)".*?\/?>/g,"[图片]") 
+    let newContent = content.replace(/<img.*?class="(.*?)(qq-emotion)(.*?)".*?\/?>/g,"[图片]") 
     newContent = newContent.replace(/<[^>]*>|<\/[^>]*>/gm,"");
 
-
     const countReturns = (newContent.match(/\n/g) || []).length;
-    let result = newContent.length;
+    let totalCount = newContent.length + IMG_SIZE * countImgs - EMOJ_SIZE * countEmojs;
     for(let i = 0; i < countReturns; i++) {
-        result += (lengthInLine - newContent.indexOf('\n') + 1);
+        totalCount += (lengthInLine - newContent.indexOf('\n') + 1);
         newContent.replace(newContent.indexOf('\n'), '');
     }
-    return result;
+    return totalCount;
 }
   
