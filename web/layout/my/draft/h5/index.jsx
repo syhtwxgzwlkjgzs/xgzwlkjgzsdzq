6ce54872f360d76ec376ac10f898ec625920f059
VS-ZX <2417818740@qq.com>
2021-06-01 17:32:31 +0800

    fix: 清除草稿箱多余底边框

--- web/layout/my/draft/h5/index.jsx
+++ web/layout/my/draft/h5/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import classNames from 'classnames';
 import styles from './index.module.scss';
 
 import Header from '@components/header';
@@ -10,9 +11,12 @@ import ThreadCenterView from '@components/thread/ThreadCenterView';
 @observer
 class Index extends React.Component {
   // 渲染单项草稿组件
-  renderItem = ({ item, onEdit }) => {
+  renderItem = ({ item, onEdit, isLast }) => {
     return (
-      <div className={styles.item} onClick={() => onEdit(item)}>
+      <div
+        className={classNames(styles.item, { [styles['border-none']]: isLast })}
+        onClick={() => onEdit(item)}
+      >
         <ThreadCenterView data={item} onClick={() => onEdit(item)} />
         <div className={styles['item-time']}>编辑于&nbsp;{item.updatedAt}</div>
       </div>
