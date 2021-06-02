a8358e8e3a7351e265e140de88d15c2665e37caa
angelzou <angelzou@tencent.com>
2021-06-02 21:56:50 +0800

    feat: 状态码处理

--- common/server/api.js
+++ common/server/api.js
@@ -8,10 +8,15 @@ import setUserAgent from '@common/utils/set-user-agent';
 import { ENV_CONFIG } from '@common/constants/site';
 import isServer from '@common/utils/is-server';
 import Router from '@discuzq/sdk/dist/router';
+import { handleError } from '@discuzq/sdk/dist/api/utils/handle-error';
 
 const api = apiIns({
   baseURL: ENV_CONFIG.COMMOM_BASE_URL && ENV_CONFIG.COMMOM_BASE_URL !== '' ? ENV_CONFIG.COMMOM_BASE_URL : isServer() ? '' : window.location.origin,
   timeout: isServer() ? 2000 : 0,
+  // 200 到 504 状态码全都进入成功的回调中
+  validateStatus(status) {
+    return status >= 200 && status <= 504;
+  },
 });
 
 const { http } = api;
@@ -51,25 +56,35 @@ http.interceptors.request.use(
 
 // 响应结果进行设置
 http.interceptors.response.use((res) => {
-  const { data } = res;
+  const { data, status, statusText } = res;
   // 如果4002将重定向到登录
   // if (data.Code === -4002) {
   //   Router.redirect({url: '/user/login'});
   // }
+  // 200 状态码
+  if (status === 200) {
+    return Promise.resolve({
+      code: data.Code,
+      data: reasetData(data.Data),
+      msg: data.Message,
+    });
+  }
   return Promise.resolve({
-    code: data.Code,
-    data: reasetData(data.Data),
-    msg: data.Message,
+    code: status,
+    data: null,
+    msg: statusText,
   });
 }, (err) => {
   if (window) {
     console.error('response', err.stack);
     console.error('response', err.message);
   }
+  // 超时的错误处理，超时的状态码是 -500
+  const { Code, Message } = handleError(err);
   return Promise.resolve({
-    code: -1,
+    code: Code,
     data: null,
-    msg: '网络发生错误',
+    msg: Code === -1 ? '网络发生错误' : Message,
   });
 });
 
