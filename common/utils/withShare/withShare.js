dd0e2d0bc6fd993298161096e765b2255e403fe9
jinyuuzhang <jinyuuzhang@tencent.com>
2021-06-02 14:21:30 +0800

    fix:格式化代码

--- common/utils/withShare/withShare.js
+++ common/utils/withShare/withShare.js
@@ -1,24 +1,23 @@
 import Taro from '@tarojs/taro';
 import Toast from '@discuzq/design/dist/components/toast';
 import goToLoginPage from '@common/utils/go-to-login-page';
-import { inject, observer } from 'mobx-react'
+import { inject, observer } from 'mobx-react';
 /**
  * @param {boolean} needShareline 是否需要分享到朋友圈
  * @param {boolean} needLogin 是否需要登录
- * @returns 
+ * @returns
  */
 function withShare(opts = {}) {
-
   // 设置默认
-  const defalutTitle = 'Discez!Q'
-  const defalutPath = 'pages/index/index'
+  const defalutTitle = 'Discez!Q';
+  const defalutPath = 'pages/index/index';
 
-  let menus = []
-  const { needShareline = true, needLogin = true } = opts
+  let menus = [];
+  const { needShareline = true, needLogin = true } = opts;
   if (needShareline) {
-    menus = ['shareAppMessage', 'shareTimeline']
+    menus = ['shareAppMessage', 'shareTimeline'];
   } else {
-    menus = ['shareAppMessage']
+    menus = ['shareAppMessage'];
   }
   return function demoComponent(Component) {
     @inject('user')
@@ -27,7 +26,7 @@ function withShare(opts = {}) {
       componentDidMount() {
         Taro.showShareMenu({
           withShareTicket: true,
-          menus: ['shareAppMessage']
+          menus,
         });
         if (super.componentDidMount) {
           super.componentDidMount();
@@ -35,36 +34,39 @@ function withShare(opts = {}) {
       }
       onShareTimeline() {
         if (this.$getShareData && typeof this.$getShareData === 'function') {
-          const shareData = this.$getShareData({ from: 'timeLine' })
-          const { title = defalutTitle, imageUrl = '' } = shareData
+          const shareData = this.$getShareData({ from: 'timeLine' });
+          const { title = defalutTitle, imageUrl = '' } = shareData;
           return {
             title,
-            imageUrl
-          }
+            imageUrl,
+          };
         }
-        return defalut
+        return {
+          defalutTitle,
+          defalutPath,
+        };
       }
       onShareAppMessage = (res) => {
-        const { user } = this.props
-        //是否必须登录
+        const { user } = this.props;
+        // 是否必须登录
         if (needLogin && !user.isLogin()) {
           Toast.info({ content: '请先登录!' });
           goToLoginPage({ url: '/subPages/user/wx-auth/index' });
-          const promise = Promise.reject()
+          const promise = Promise.reject();
           return {
-            promise
-          }
+            promise,
+          };
         }
-        const data = res.target?.dataset?.shareData || ''
-        let shareData = ''
+        const data = res.target?.dataset?.shareData || '';
+        let shareData = '';
         if (this.$getShareData && typeof this.$getShareData === 'function') {
-          shareData = this.$getShareData({ ...data, from: res.from })
+          shareData = this.$getShareData({ ...data, from: res.from });
         }
-        const { title = defalutTitle, path = defalutPath } = shareData
+        const { title = defalutTitle, path = defalutPath } = shareData;
         return {
           title,
-          path
-        }
+          path,
+        };
       }
 
       render() {
