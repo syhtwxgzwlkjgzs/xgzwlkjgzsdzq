794646c241166888d31be8afa4205908ee5e852c
kevinchou <kevinchou@tencent.com>
2021-06-02 18:01:24 +0800

    feat: 小程序我的钱包功能移植

--- mini/src/layout/wallet/components/pay-list/index.jsx
+++ mini/src/layout/wallet/components/pay-list/index.jsx
@@ -1,41 +1,46 @@
 import React from 'react';
-import { inject, observer } from 'mobx-react';
+import { observer } from 'mobx-react';
 import { View } from '@tarojs/components';
 import { diffDate } from '@common/utils/diff-date';
+import { time } from '@discuzq/sdk/dist/index';
 
 import styles from './index.module.scss';
 
-
+const PAY_STATUS_MAP = {
+  0: '待付款',
+  1: '已付款',
+  2: '取消订单',
+  3: '支付失败',
+  4: '订单已过期',
+  5: '部分退款',
+  10: '全额退款',
+  11: '异常订单',
+};
 
 @observer
 class IncomeList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.payStatus = true ;
-
-    this.showText = [
-      '注册支出',
-      '人工支出',
-      '打赏了主题', // 打赏支出
-      '付费查看了主题', // 付费支出
-      '红包支出',
-      '悬赏支出',
-    ];
+    this.payStatus = true;
   }
 
   render() {
+    const { itemKey, dataLength } = this.props;
     return (
-        <View className={styles.container}>
-            <View className={styles.content}>
-                <View className={styles.text}>{this.showText[this.props.payVal.type]}</View>
-                <View className={styles.money}>-{this.props.payVal.money}</View>
-            </View>
-            <View className={styles.footer}>
-              <View className={styles.time}>{diffDate(this.props.payVal.time)}</View>
-              <View className={`${this.props.payVal.payStatus ? styles.payStatusTrue : styles.payStatusFalse}`}>{this.props.payVal.payStatus ? '已付款' : '待付款'}</View>
-            </View>
+      <View className={styles.container}>
+        <View className={styles.content}>
+          <View className={styles.text}>{this.props.payVal.title || this.props.payVal.changeDesc}</View>
+          <View className={styles.money}>{this.props.payVal.amount}</View>
         </View>
+        {/* // FIXME:这里的结构有问题 怪怪的 所以只能用数组长度取消底部边框线 */}
+        <View className={styles.footer} style={{ borderBottom: itemKey === dataLength - 1 && 0 }}>
+          <View className={styles.time}>{diffDate(time.formatDate(this.props.payVal.createdAt, 'YYYY-MM-DD'))}</View>
+          <View className={`${this.props.payVal.status ? styles.payStatusTrue : styles.payStatusFalse}`}>
+            {PAY_STATUS_MAP[this.props.payVal.status]}
+          </View>
+        </View>
+      </View>
     );
   }
 }
