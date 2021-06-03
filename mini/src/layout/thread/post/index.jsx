07712d5bd945bdbab53d2ad661b7b7cfeae1ea48
angelzou <angelzou@tencent.com>
2021-06-03 16:57:06 +0800

    refactor: 小程序滚动问题[待解决]

--- mini/src/layout/thread/post/index.jsx
+++ mini/src/layout/thread/post/index.jsx
@@ -194,6 +194,10 @@ class Index extends Component {
     // 匹配附件、图片、语音上传
     this.setState({
       operationType: item.type
+    }, () => {
+      // if (item.type === THREAD_TYPE.file || item.type === THREAD_TYPE.image || item.type === THREAD_TYPE.voice) {
+      //   this.scrollerIntoView();
+      // }
     });
 
     if (item.type !== 'emoji') {
@@ -259,7 +263,6 @@ class Index extends Component {
         });
         break;
     }
-
     if (nextRoute) Taro.navigateTo({ url: nextRoute });
 
   }
@@ -338,6 +341,7 @@ class Index extends Component {
             duration: 2000
           });
         }
+        // this.scrollerIntoView();
       },
       // 上传错误回调，处理异常
       error: function (result) {
@@ -543,6 +547,25 @@ class Index extends Component {
     url ? Taro.redirectTo({ url }) : Taro.navigateBack();
   }
 
+  // scrollerIntoView() {
+  //   const contentId = '#thread-post-content';
+  //   const query = Taro.createSelectorQuery();
+  //   query.select(contentId).boundingClientRect();
+  //   query.selectViewport().scrollOffset()
+  //   query.exec((res) => {
+  //     const { bottom } = res[0] || {};
+  //     const scrollTop = bottom + 200;
+  //     Taro.pageScrollTo({
+  //       scrollTop,
+  //       selector: contentId,
+  //       duration: 300,
+  //       complete: (a, b, c) => {
+  //         console.log(a,b,c)
+  //       }
+  //     });
+  //   })
+  // }
+
   render() {
     const { permissions } = this.props.user;
     const { categories } = this.props.index;
@@ -578,6 +601,7 @@ class Index extends Component {
 
           {/* 内容区域，inclue标题、帖子文字、图片、附件、语音等 */}
           <View className={styles['content']} style={contentStyle}>
+            {/* <View id="thread-post-content"> */}
             <Title
               value={postData.title}
               show={isShowTitle}
@@ -624,6 +648,7 @@ class Index extends Component {
               )}
 
             </View>
+            {/* </View> */}
           </View>
 
           {/* 插入内容tag展示区 */}
@@ -697,6 +722,7 @@ class Index extends Component {
               operationType={operationType}
               permissions={permissions}
               onPluginClick={(item) => {
+                console.log(item);
                 this.handlePluginClick(item);
               }}
               onSubmit={() => this.handleSubmit()}
