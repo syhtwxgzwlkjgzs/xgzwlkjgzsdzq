7fc9aaaca1007854129b9753bc4774be3a6a0253
LLLLLamHo <linhao.4zhong@163com>
2021-06-04 17:53:04 +0800

    test

--- common/config/sentry.js
+++ common/config/sentry.js
@@ -3,5 +3,10 @@ export default {
     dsn: 'https://148faceea8c84ee4a465d3217652d8e3@report.url.cn/sentry/3194',
     release: version,
     debug: process.env.NODE_ENV === 'production' ? false : true,
-    environment: process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
+    environment: process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
+    transportOptions: {
+        fetchParameters: {
+          mode: 'no-cors'
+        }
+    }
 }
