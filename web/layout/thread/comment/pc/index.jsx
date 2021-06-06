5a445e817b78aedbc92f9909e682e7dd1e58afae
kevinchou <kevinchou@tencent.com>
2021-06-06 13:46:03 +0800

    fix: 评论内容非空输入类型校验

--- web/layout/thread/comment/pc/index.jsx
+++ web/layout/thread/comment/pc/index.jsx
@@ -187,7 +187,9 @@ class CommentPCPage extends React.Component {
       return;
     }
 
-    if (!val) {
+    const valuestr = val.replace(/\s/g, '');
+    // 如果内部为空，且只包含空格或空行
+    if (!valuestr) {
       Toast.info({ content: '请输入内容' });
       return;
     }
