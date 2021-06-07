f71edc3c76bfe6e188e4414f9afc7d03a51684d3
fishcui <fishcui@tencent.com>
2021-06-07 20:34:47 +0800

    feat: 小程序上传头像和背景逻辑

--- mini/src/components/user-center-edit-header/index.jsx
+++ mini/src/components/user-center-edit-header/index.jsx
@@ -1,91 +1,157 @@
 import React, { Component } from 'react';
+import Taro from '@tarojs/taro';
 import styles from './index.module.scss';
 import Avatar from '@components/avatar';
 import UserCenterHeaderImage from '@components/user-center-header-images';
 import Icon from '@discuzq/design/dist/components/icon/index';
 import Input from '@discuzq/design/dist/components/input/index';
 import { inject, observer } from 'mobx-react';
-import { ACCEPT_IMAGE_TYPES } from '@common/constants/thread-post'
+import { ACCEPT_IMAGE_TYPES } from '@common/constants/thread-post';
 import { View, Text } from '@tarojs/components';
-
+import locals from '@common/utils/local-bridge';
+import getConfig from '@common/config';
+import constants from '@common/constants';
+import Toast from '@discuzq/design/dist/components/toast';
 
 @inject('user')
 @observer
 export default class index extends Component {
+  config = {};
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      isClickSignature: false
-    }
-    this.user = this.props.user || {}
+      isClickSignature: false,
+    };
+    this.config = getConfig();
+    this.user = this.props.user || {};
   }
-
-  avatarUploaderRef = React.createRef(null);
-  backgroundUploaderRef = React.createRef(null);
 
   handleAvatarUpload = () => {
-    this.avatarUploaderRef.current.click();
-  }
+    Taro.chooseImage({
+      count: 1,
+      success: (res) => {
+        this.onAvatarChange(res.tempFiles);
+      },
+    });
+  };
 
   handleBackgroundUpload = () => {
-    this.backgroundUploaderRef.current.click();
+    Taro.chooseImage({
+      count: 1,
+      success: (res) => {
+        this.onBackgroundChange(res.tempFiles);
+      },
+    });
+  };
+
+  uploadAvatarImpl = async (fileList) => {
+    const token = locals.get(constants.ACCESS_TOKEN_NAME);
+    const uploadRes = await Taro.uploadFile({
+      url: `${this.config.COMMOM_BASE_URL}/apiv3/users/avatar`,
+      filePath: fileList[0].path,
+      header: {
+        'Content-Type': 'multipart/form-data',
+        authorization: `Bearer ${token}`,
+      },
+      name: 'avatar',
+    });
+    try {
+      const parsedData = JSON.parse(uploadRes.data);
+      console.log(parsedData);
+      if (parsedData.Code === 0) {
+        Toast.success({
+          content: '上传成功',
+          duration: 2000
+        });
+      } else {
+        Toast.error({
+          content: parsedData.Message,
+          duration: 2000
+        })
+      }
+    } catch (e) {
+      console.log(e);
+      Toast.error({
+        content: '解析失败',
+        duration: 2000
+      })
+    }
+  };
+
+  uploadBackgroundImpl = async (fileList) => {
+    const token = locals.get(constants.ACCESS_TOKEN_NAME);
+    const uploadRes = await Taro.uploadFile({
+      url: `${this.config.COMMOM_BASE_URL}/apiv3/users/background`,
+      filePath: fileList[0].path,
+      header: {
+        'Content-Type': 'multipart/form-data',
+        authorization: `Bearer ${token}`,
+      },
+      name: 'background',
+    });
+    console.log(uploadRes.data)
   }
 
-  onAvatarChange = (fileList) => {
-    this.props.user.updateAvatar(fileList.target.files);
-  }
+  onAvatarChange = async (fileList) => {
+    await this.uploadAvatarImpl(fileList);
+  };
 
-  onBackgroundChange = (fileList) => {
-    this.props.user.updateBackground(fileList.target.files);
-  }
+  onBackgroundChange = async (fileList) => {
+    await this.uploadBackgroundImpl(fileList);
+  };
 
   // 点击编辑签名
   handleClickSignature = () => {
     this.setState({
-      isClickSignature: !this.state.isClickSignature
-    })
-  }
+      isClickSignature: !this.state.isClickSignature,
+    });
+  };
 
   // 签名change事件
   handleChangeSignature = (e) => {
-    this.props.user.editSignature = e.target.value
-  }
+    this.props.user.editSignature = e.target.value;
+  };
 
   handleBlurSignature = (e) => {
-    this.props.user.editSignature = e.target.value
+    this.props.user.editSignature = e.target.value;
     this.setState({
-      isClickSignature: false
-    })
-  }
+      isClickSignature: false,
+    });
+  };
 
   render() {
     return (
       <>
         <View className={styles.userCenterEditHeader}>
           <UserCenterHeaderImage onClick={this.handleBackgroundUpload} />
-          <input onChange={this.onBackgroundChange} ref={this.backgroundUploaderRef} type="file" style={{ display: 'none' }} />
           <View className={styles.headImgBox}>
-            <Avatar image={this.user.avatarUrl} size='big' name={this.user.username} />
+            <Avatar image={this.user.avatarUrl} size="big" name={this.user.username} />
             {/* 相机图标 */}
             <View className={styles.userCenterEditCameraIcon} onClick={this.handleAvatarUpload}>
               <Icon name="CameraOutlined" />
-              <input onChange={this.onAvatarChange} ref={this.avatarUploaderRef} type="file" style={{ display: 'none' }} multiple={false} accept={ACCEPT_IMAGE_TYPES.join(',')} />
             </View>
           </View>
           {/* 编辑修改说明 */}
           <View className={styles.userCenterEditDec}>
             <Icon onClick={this.handleClickSignature} name="CompileOutlined" />
-            {
-              this.state.isClickSignature ? (
-                <Text className={styles.text}><Input maxLength={20} focus={true} onChange={this.handleChangeSignature} onBlur={this.handleBlurSignature} value={this.user.editSignature} placeholder="这个人很懒，什么也没留下~" /></Text>
-              ) : (
-                <Text className={styles.text}>{this.user.editSignature || '这个人很懒，什么也没留下~'}</Text>
-              )
-            }
+            {this.state.isClickSignature ? (
+              <View className={styles.text}>
+                <Input
+                  maxLength={20}
+                  focus={true}
+                  onChange={this.handleChangeSignature}
+                  onBlur={this.handleBlurSignature}
+                  value={this.user.editSignature}
+                  placeholder="这个人很懒，什么也没留下~"
+                />
+              </View>
+            ) : (
+              <Text className={styles.text}>{this.user.editSignature || '这个人很懒，什么也没留下~'}</Text>
+            )}
           </View>
         </View>
       </>
-    )
+    );
   }
 }
--- mini/src/components/user-center-edit-info/index.jsx
+++ mini/src/components/user-center-edit-info/index.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import UserCenterEditHeader from '../user-center-edit-header'
+import React, { Component } from 'react';
+import UserCenterEditHeader from '../user-center-edit-header';
 import Button from '@discuzq/design/dist/components/button/index';
 import Icon from '@discuzq/design/dist/components/icon/index';
 import Input from '@discuzq/design/dist/components/input/index';
@@ -7,92 +7,104 @@ import styles from './index.module.scss';
 import Avatar from '@components/avatar';
 import { inject, observer } from 'mobx-react';
 import Router from '@discuzq/sdk/dist/router';
-// import HOCFetchSiteData from '../../../middleware/HOCFetchSiteData'
 import { View, Text } from '@tarojs/components';
+
 @inject('site')
 @inject('user')
 @observer
 class index extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      isClickNickName: false
-    }
-    this.user = this.props.user || {}
+      isClickNickName: false,
+    };
+    this.user = this.props.user || {};
   }
 
   initState = () => {
     this.setState({
-      isClickNickName: false
-    })
-  }
+      isClickNickName: false,
+    });
+  };
 
   componentDidMount() {
-    this.initState()
-    this.props.user.initEditInfo()
+    this.initState();
+    this.props.user.initEditInfo();
   }
 
   // 点击取消
   handleCancel = () => {
-    Router.back()
-  }
+    Router.back();
+  };
 
   handleClickNickName = () => {
     this.setState({
-      isClickNickName: !this.state.isClickNickName
-    })
-  }
+      isClickNickName: !this.state.isClickNickName,
+    });
+  };
 
   handleChangeNickName = (e) => {
-    let value = e.target.value
-    this.props.user.editNickName = value
-  }
+    let value = e.target.value;
+    this.props.user.editNickName = value;
+  };
 
   handleBlurNickName = (e) => {
-    let value = e.target.value
-    this.props.user.editNickName = value
+    let value = e.target.value;
+    this.props.user.editNickName = value;
     this.setState({
-      isClickNickName: false
-    })
-  }
+      isClickNickName: false,
+    });
+  };
 
   // 渲染修改用户名
   renderInputNickName = () => {
-    const { isClickNickName } = this.state
+    const { isClickNickName } = this.state;
     return (
       <View className={styles.userCenterEditLabel}>
         <Text>昵称</Text>
-        <View>{isClickNickName ? <Input focus={true} maxLength={10} value={this.user.editNickName} onChange={this.handleChangeNickName} onBlur={this.handleBlurNickName} /> : this.user.editNickName}</View>
+        <View>
+          {isClickNickName ? (
+            <Input
+              focus={true}
+              maxLength={10}
+              value={this.user.editNickName}
+              onChange={this.handleChangeNickName}
+              onBlur={this.handleBlurNickName}
+            />
+          ) : (
+            this.user.editNickName
+          )}
+        </View>
       </View>
-    )
-  }
+    );
+  };
 
   handleUpdateEditedUserInfo = () => {
-    this.props.user.updateEditedUserInfo()
-    Router.push({url: `/my`})
-  }
+    this.props.user.updateEditedUserInfo();
+    Router.push({ url: `/my` });
+  };
 
   handleGoToEditMobile = () => {
-    console.log('去到修改手机号页面');
-    Router.push({url: `mobile/index`})
-  }
+    Router.push({ url: `mobile/index` });
+  };
 
   handleGoToEditAccountPwd = () => {
-    console.log('去到修改账户密码页面');
-    Router.push({url: `pwd/index`})
-  }
+    Router.push({ url: `pwd/index` });
+  };
 
   handleGoToEditPayPwd = () => {
-    console.log('去到修改支付密码页面');
-    Router.push({url: 'paypwd/index'})
-  }
+    Router.push({ url: 'paypwd/index' });
+  };
 
   render() {
+    // 条件都满足时才显示微信
+    const IS_WECHAT_ACCESSABLE = this.props.site.wechatEnv !== 'none' && !!this.user.wxNickname;
     return (
       <View>
         {/* 头部 */}
-        <View><UserCenterEditHeader /></View>
+        <View>
+          <UserCenterEditHeader />
+        </View>
         {/* middle */}
         <View className={styles.userCenterEditMiddle}>
           <View className={styles.title}>个人信息</View>
@@ -105,40 +117,45 @@ class index extends Component {
               <View>{this.user.editUserName}</View>
             </View>
           </View>
-          <View className={styles.userCenterEditItem}>
-            <View className={styles.userCenterEditLabel}>
-              <Text>手机号码</Text>
-              <View>{this.user.mobile}</View>
+          {this.props.site?.isSmsOpen && (
+            <View className={styles.userCenterEditItem}>
+              <View className={styles.userCenterEditLabel}>
+                <Text>手机号码</Text>
+                <View>{this.user.mobile || '去绑定'}</View>
+              </View>
+
+              <View className={styles.gotoIcon} onClick={this.handleGoToEditMobile}>
+                <Icon name="RightOutlined" />
+              </View>
             </View>
-            <View onClick={this.handleGoToEditMobile}><Icon name="RightOutlined" /></View>
-            {/* {
-              this.props.site.isSmsOpen && (
-                <View><Icon name="RightOutlined" /></View>
-              )
-            } */}
-          </View>
+          )}
           <View className={styles.userCenterEditItem}>
             <View className={styles.userCenterEditLabel}>
               <Text>账户密码</Text>
               <View>修改</View>
             </View>
-            <View onClick={this.handleGoToEditAccountPwd}><Icon name="RightOutlined" /></View>
+            <View className={styles.gotoIcon} onClick={this.handleGoToEditAccountPwd}>
+              <Icon name="RightOutlined" />
+            </View>
           </View>
           <View className={styles.userCenterEditItem}>
             <View className={styles.userCenterEditLabel}>
               <Text>支付密码</Text>
               <View>修改</View>
             </View>
-            <View onClick={this.handleGoToEditPayPwd}><Icon name="RightOutlined" /></View>
+            <View className={styles.gotoIcon} onClick={this.handleGoToEditPayPwd}>
+              <Icon name="RightOutlined" />
+            </View>
           </View>
           <View className={styles.userCenterEditItem} style={{ border: 'none' }}>
             <View className={styles.userCenterEditLabel}>
               <Text>微信</Text>
-              <View className={styles.userCenterEditWeChat}>{
-                this.user.unionid ? <>
-                  <Avatar size="small" image={this.user.avatarUrl} name={this.user.username} /> <Text>{this.user.nickname}（解绑）</Text>
-                </> : '暂未绑定'
-              }</View>
+              <View className={styles.userCenterEditWeChat}>
+                <Avatar size="small" image={this.user.avatarUrl} name={this.props.user.username} />{' '}
+                <Text>
+                  {this.props.user.wxNickname}
+                </Text>
+              </View>
             </View>
           </View>
         </View>
@@ -154,20 +171,16 @@ class index extends Component {
           </View> */}
           <View className={styles.userCenterEditBtn}>
             <Button onClick={this.handleCancel}>
-              <View className={styles.actionButtonContentWrapper}>
-                取消
-              </View>
+              <View className={styles.actionButtonContentWrapper}>取消</View>
             </Button>
             <Button onClick={this.handleUpdateEditedUserInfo} type="primary">
-              <View className={styles.actionButtonContentWrapper}>
-                保存
-              </View>
+              <View className={styles.actionButtonContentWrapper}>保存</View>
             </Button>
           </View>
         </View>
       </View>
-    )
+    );
   }
 }
 
-export default index
+export default index;
