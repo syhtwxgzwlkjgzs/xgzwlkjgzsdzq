ee36d1f0b1c97997a83ee1057b5b278e1aae7d57
LLLLLamHo <linhao.4zhong@163com>
2021-06-09 23:06:36 +0800

    test

--- web/next.config.js
+++ web/next.config.js
@@ -3,5 +3,10 @@ const nextConfig = require('@discuzq/cli/config/next');
 module.exports = nextConfig((config) => {
     config.productionBrowserSourceMaps = true;
     config.crossOrigin = 'anonymous';
+    // config.devIndicators = {
+    //     autoPrerender: false,
+    // }
+    // config.plugins.push(new BundleAnalyzerPlugin({ analyzerPort: 8919 }));
+
     return config;
 });
