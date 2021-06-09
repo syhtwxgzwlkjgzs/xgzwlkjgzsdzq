1b6417a8d71ef47bfc5c215b923ae04bbe62c810
v_zjxzzheng <v_zjxzzheng@tencent.com>
2021-06-09 18:26:28 +0800

    fix: 添加title

--- web/pages/forum/partner-invite/index.jsx
+++ web/pages/forum/partner-invite/index.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ParnerInviteH5Page from '@layout/forum/h5/partner-invite';
 import { withRouter } from 'next/router';
 import { inject } from 'mobx-react';
+import ViewAdapter from '@components/view-adapter';
 
 import HOCFetchSiteData from '@middleware/HOCFetchSiteData';
 
 @inject('site')
 class ParnerInvite extends React.Component {
   render() {
-    return <ParnerInviteH5Page />;
+    const { router } = this.props;
+    const inviteCode = router?.query?.inviteCode;
+    return <ViewAdapter
+              h5={<ParnerInviteH5Page />}
+              pc={<ParnerInviteH5Page />}
+              title={`${inviteCode ? '邀请进站' : '站点加入'} - ${this.props.site?.siteName}`}
+            />;
   }
 }
 
