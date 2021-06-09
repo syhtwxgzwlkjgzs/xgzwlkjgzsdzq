5549d6b9379c736cd7440ef7da11145074f73569
joneeeeli <joneeeeli@tencent.com>
2021-06-09 11:26:26 +0800

    fix

--- mini/src/components/unread-red-dot/index.jsx
+++ mini/src/components/unread-red-dot/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from '@tarojs/components';
 import styles from './index.module.scss';
 import classNames from 'classnames';
-import Badge from '@discuzq/design/components/badge/index';
+import Badge from '@discuzq/design/dist/components/badge/index';
 
 
 const Index = ({ children, unreadCount, type = '', style}) => {
