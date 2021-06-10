25c8b13d4de3919b5e682537629ce56333b93a88
fishcui <fishcui@tencent.com>
2021-06-10 15:30:37 +0800

    fix: H5端 title 修正

--- web/pages/my/edit/username/index.jsx
+++ web/pages/my/edit/username/index.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import UserCenterEditUserName from '../../../../components/user-center-edit-username/index';
 import HOCUserInfo from '@middleware/HOCUserInfo';
 import HOCFetchSiteData from '@middleware/HOCFetchSiteData';
+import ViewAdapter from '@components/view-adapter';
+import { inject, observer } from 'mobx-react';
 
-function index() {
-  return (
-    <div>
-      <UserCenterEditUserName />
-    </div>
-  );
+@inject('site')
+@observer
+class EditUserNamePage extends React.Component {
+  render() {
+    return <ViewAdapter 
+    h5={(
+      <div>
+        <UserCenterEditUserName />
+      </div>
+    )}
+    pc={null} 
+    title={`编辑用户名 - ${this.props.site?.siteName}`}
+  />
+  }
 }
 
-export default HOCFetchSiteData(HOCUserInfo(index));
+export default HOCFetchSiteData(HOCUserInfo(EditUserNamePage));
