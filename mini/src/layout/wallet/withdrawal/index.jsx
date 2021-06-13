dd05568906e913489603b3e5332f7c53d023c299
fishcui <fishcui@tencent.com>
2021-06-13 22:36:07 +0800

    fix: 修复他人主页的加载问题 & 钱包的提现加载问题

--- mini/src/layout/wallet/withdrawal/index.jsx
+++ mini/src/layout/wallet/withdrawal/index.jsx
@@ -6,6 +6,7 @@ import { View } from '@tarojs/components';
 import classNames from 'classnames';
 import MoneyInput from './components/money-input';
 import styles from './index.module.scss';
+import Taro from '@tarojs/taro';
 
 @inject('wallet')
 @inject('site')
@@ -77,12 +78,14 @@ class Withdrawal extends React.Component {
       .createWalletCash({
         money: this.state.withdrawalAmount,
       })
-      .then(() => {
+      .then(async () => {
         Toast.success({
           content: '申请提现成功',
           hasMask: false,
           duration: 2000,
         });
+        const { getUserWalletInfo } = this.props.wallet;
+        await getUserWalletInfo();
         Taro.navigateBack();
         this.initState();
       })
