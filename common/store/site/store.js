7cd443530543b8f7005379079d398c38ed622d96
leohaozhang <leohaozhang@tencent.com>
2021-06-16 10:31:55 +0800

    fix: 修复邀请加入页站点介绍缺失时默认提示

--- common/store/site/store.js
+++ common/store/site/store.js
@@ -53,6 +53,11 @@ class SiteStore {
     return get(this.webConfig, 'setSite.siteName');
   }
 
+  // 站点介绍
+  @computed get siteIntroduction() {
+    return get(this.webConfig, 'setSite.siteIntroduction', '暂无介绍');
+  }
+
   // 注册协议开关
   @computed get isAgreementRegister() {
     return get(this.webConfig, 'agreement.register', true);
