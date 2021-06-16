452ab3a140977e155c8152eadae4c26f19f0221d
Sean Luo <luoxinran@eims.com.cn>
2021-06-16 21:08:58 +0800

    还原到初始preload

--- web/layout/index/h5/index.jsx
+++ web/layout/index/h5/index.jsx
@@ -191,7 +191,6 @@ class IndexH5Page extends React.Component {
         quickScroll={true}
         curr='home'
         pageName='home'
-        preload={3000}
         onClickTabBar={this.onClickTabBar}
         requestError={threadError.isError}
         errorText={threadError.errorText}
