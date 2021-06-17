cfe528031ef51c5666dc0fd5ac736beed7bfbaeb
jf19981111 <18270711420@163.com>
2021-06-17 17:52:40 +0800

    fix: 钱包提现toast

--- web/layout/wallet/withdrawal/index.jsx
+++ web/layout/wallet/withdrawal/index.jsx
@@ -17,21 +17,21 @@ class Withdrawal extends React.Component {
       visible: true,
       moneyOverThanAmount: false, // 是否超过当前可提现金额
       withdrawalAmount: 0,
-      inputValue: "", // 金额输入内容
+      inputValue: '', // 金额输入内容
     };
   }
 
-  updateState = ({name, value}) => {
+  updateState = ({ name, value }) => {
     this.setState({
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
   onChange = (data) => {
     const datas = data.match(/([1-9]\d{0,9}|0)(\.\d{0,2})?/);
     this.setState({
-      inputValue: datas ? datas[0] : ''
-    })
+      inputValue: datas ? datas[0] : '',
+    });
     this.getmoneyNum(datas ? datas[0] : '');
   };
 
@@ -40,9 +40,9 @@ class Withdrawal extends React.Component {
       visible: true,
       moneyOverThanAmount: false, // 是否超过当前可提现金额
       withdrawalAmount: 0,
-      inputValue: ""
-    })
-  }
+      inputValue: '',
+    });
+  };
 
   // 获得输入的提现金额
   getmoneyNum = (data) => {
@@ -66,34 +66,41 @@ class Withdrawal extends React.Component {
 
   // 提现到微信钱包
   moneyToWeixin = async () => {
-    if (!this.state.withdrawalAmount || parseFloat(this.state.withdrawalAmount) < parseFloat(this.props.site.cashMinSum)) {
+    if (
+      !this.state.withdrawalAmount ||
+      parseFloat(this.state.withdrawalAmount) < parseFloat(this.props.site.cashMinSum)
+    ) {
       return Toast.warning({ content: '不得小于最低提现金额' });
     }
 
-    this.props.wallet.createWalletCash({
-      money: this.state.withdrawalAmount,
-    }).then(res => {
-      Toast.success({
-        content: res.Msg || '申请提现成功',
-        hasMask: false,
-        duration: 1000,
+    this.props.wallet
+      .createWalletCash({
+        money: this.state.withdrawalAmount,
       })
-      this.initState();
-    }).catch(err => {
-      if (err.Code) {
-        Toast.error({
-          content: err.Msg || "申请提现失败，请重试",
-          duration: 1000,
+      .then((res) => {
+        Toast.success({
+          content: res.Msg || '申请提现成功',
+          hasMask: false,
+          duration: 2000,
         });
-      }
-      this.initState();
-    });
+        this.initState();
+      })
+      .catch((err) => {
+        if (err.Code) {
+          Toast.error({
+            content: err.Msg || '申请提现失败，请重试',
+            duration: 2000,
+          });
+        }
+        this.initState();
+      });
     // this.setState({ visible: !this.state.visible });
   };
 
   render() {
     const { inputValue } = this.state;
-    const btnDisabled = !inputValue || parseFloat(this.state.withdrawalAmount) > parseFloat(this.props.wallet?.availableAmount)
+    const btnDisabled =
+      !inputValue || parseFloat(this.state.withdrawalAmount) > parseFloat(this.props.wallet?.availableAmount);
     return (
       <>
         <Header />
@@ -115,18 +122,14 @@ class Withdrawal extends React.Component {
               />
             </div>
           </div>
-          <div className={
-            classNames(styles.footer,{
-              [styles.bgBtnColor]: !btnDisabled
-            })
-          }>
-            <Button
-              type={'primary'}
-              className={styles.button}
-              onClick={this.moneyToWeixin}
-              disabled={btnDisabled}>
+          <div
+            className={classNames(styles.footer, {
+              [styles.bgBtnColor]: !btnDisabled,
+            })}
+          >
+            <Button type={'primary'} className={styles.button} onClick={this.moneyToWeixin} disabled={btnDisabled}>
               提现到微信钱包
-          </Button>
+            </Button>
           </div>
         </div>
       </>
