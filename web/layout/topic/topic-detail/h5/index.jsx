7f5d7e1cf33bfea11dd39433d17982c8d3ab48fe
jinyuuzhang <jinyuuzhang@tencent.com>
2021-06-18 14:45:37 +0800

    优化loading样式

--- web/layout/topic/topic-detail/h5/index.jsx
+++ web/layout/topic/topic-detail/h5/index.jsx
@@ -57,7 +57,7 @@ class TopicH5Page extends React.Component {
       <BaseLayout allowRefresh={false}>
         {
           fetchTopicInfoLoading ? (
-            <BottomView loadingText='加载中...' isError={isError} errorText={errorText} />
+            <BottomView loadingText='加载中...' isBox isError={isError} errorText={errorText} />
           )
             : (
               pageData?.map((item, index) => (
