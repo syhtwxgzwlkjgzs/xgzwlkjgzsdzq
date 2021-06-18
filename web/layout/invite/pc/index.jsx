6d2c3bcea19ddaafdae7207a0faea609f8644690
angelzou <angelzou@tencent.com>
2021-06-18 13:18:30 +0800

    feat: 调整个人中心和邀请页的自适应问题

--- web/layout/invite/pc/index.jsx
+++ web/layout/invite/pc/index.jsx
@@ -11,7 +11,7 @@ import UserCenterFriendPc from '@components/user-center/friend-pc';
 import { numberFormat } from '@common/utils/number-format';
 import Copyright from '@components/copyright';
 import { copyToClipboard } from '@common/utils/copyToClipboard';
-import h5Share from '@discuzq/sdk/dist/common_modules/share/h5';
+import UserInfo from './components/userinfo';
 
 @inject('site')
 @inject('forum')
@@ -51,22 +51,6 @@ class InvitePCPage extends React.Component {
     return await invite.getInviteUsersList(invite.currentPage + 1);
   };
 
-  createInviteLink = async () => {
-    try {
-      const { site: { setSite: { siteTitle } = {} } = {}, user } = this.props;
-      // copyToClipboard(`${window.location.origin}/forum/partner-invite?inviteCode=${user.id}`);
-      h5Share({ title: `邀请您加入${siteTitle || ''}`, path: `/forum/partner-invite?inviteCode=${user.id}` });
-      Toast.success({
-        content: '创建邀请链接成功',
-        duration: 1000,
-      });
-    } catch (e) {
-      Toast.error({
-        content: e.Message || e,
-      });
-    }
-  }
-
   contentHeader = () => <div className={layout.content_header}></div>;
 
   // 右侧 - 潮流话题 粉丝 版权信息
@@ -75,34 +59,9 @@ class InvitePCPage extends React.Component {
     const { pageData = [] } = this.props.search.topics || { pageData: [] };
     return (
       <>
-        <div className={layout.user_card_wrap}>
-          <div className={layout.user_card_main}>
-            <div className={layout.user_card_avatar}>
-              <Avatar
-                size={'big'}
-                image={inviteData.avatar}
-                text={inviteData.nickname && inviteData.nickname.substring(0, 1)}
-              />
-            </div>
-            <div className={layout.user_card_info}>
-              <div className={layout.user_info_name} title={inviteData.nickname}>{inviteData.nickname}</div>
-              <div className={layout.user_info_tag}>{inviteData.groupName}</div>
-              <div className={layout.user_info_invite}>
-                <div className={layout.invite_num}>
-                  <div className={layout.invite_num_title}>已邀人数</div>
-                  <div className={layout.invite_num_content} title={numberFormat(inviteData.totalInviteUsers)}>{numberFormat(inviteData.totalInviteUsers)}</div>
-                </div>
-                <div className={layout.invite_money}>
-                  <div className={layout.invite_num_title}>赚得赏金</div>
-                  <div className={layout.invite_num_content} title={inviteData.totalInviteBounties}>{inviteData.totalInviteBounties || 0}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={layout.user_card_button} onClick={this.createInviteLink}>邀请朋友</div>
-        </div>
+        <UserInfo />
         <UserCenterFriendPc className={layout.user_center_wrap}/>
-        <UserCenterFansPc  className={layout.user_center_wrap}/>
+        <UserCenterFansPc  className={`${layout.user_center_wrap} invite-pc`}/>
         <Copyright/>
       </>
     );
