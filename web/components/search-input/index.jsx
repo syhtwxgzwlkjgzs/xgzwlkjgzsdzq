287589e20c035e41da419247fd529fea499a27b7
Sean Luo <luoxinran@eims.com.cn>
2021-06-18 17:19:42 +0800

    阻止搜索期间按Enther

--- web/components/search-input/index.jsx
+++ web/components/search-input/index.jsx
@@ -26,6 +26,7 @@ const SearchInput = ({
   const [value, setValue] = React.useState(defaultValue);
   const [isShow, setIsShow] = React.useState(false);
   const [timeoutID, setTimeoutID] = React.useState(null);
+
   const inputChange = (e) => {
     const val = e.target.value;
     setValue(val);
@@ -42,10 +43,17 @@ const SearchInput = ({
       }, searchWhileTyping ? 1000 : 0));
     }
   }
+
+  const onEnter = (e) => {
+    if(timeoutID !== null) return;
+    onSearch(e.target.value)
+  }
+
   const clearInput = () => {
     setValue('');
     setIsShow(false)
   }
+
   return (
     <div className={`${styles.container} ${!isShowBottom && styles.hiddenBottom}`}>
       <div className={styles.inputWrapper}>
@@ -53,7 +61,7 @@ const SearchInput = ({
         <Input
           value={value}
           placeholder='请输入想要搜索的内容...'
-          onEnter={e => onSearch(e.target.value)}
+          onEnter={e => onEnter(e)}
           onChange={e => inputChange(e)}
           className={styles.input}
         />
