3f0516c6bfc52ad20af48e330702e69373c319da
LLLLLamHo <linhao.4zhong@163com>
2021-06-19 21:32:00 +0800

    test

--- mini/src/subPages/forum/partner-invite/index.jsx
+++ mini/src/subPages/forum/partner-invite/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ParnerInviteH5Page from '@layout/forum/partner-invite';
-
+import Taro from '@tarojs/taro';
 import Page from '@components/page';
 
 class ParnerInvite extends React.Component {
+  componentDidMount() {
+    Taro.hideHomeButton();
+  }
   render() {
     return (
       <Page>
