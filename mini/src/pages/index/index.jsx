62983f888d8b0890435899a18d315292ce372ce7
LLLLLamHo <linhao.4zhong@163com>
2021-06-20 14:24:45 +0800

    tets

--- mini/src/pages/index/index.jsx
+++ mini/src/pages/index/index.jsx
@@ -148,6 +148,7 @@ class Index extends React.Component {
         // 带有指定的路径，将不去首页。
         const $instance = Taro.getCurrentInstance()
         const router = $instance.router;
+        console.log(router)
         if (router.params && router.params.path) {
           Router.redirect({
             url: decodeURIComponent(router.params.path),
