d527d7eff616e7f3fd4d4be498aeead1eb81675a
joneeeeli <joneeeeli@tencent.com>
2021-06-21 16:56:56 +0800

    未读消息更新优化

--- web/components/header/h5/index.jsx
+++ web/components/header/h5/index.jsx
@@ -1,28 +1,44 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { inject, observer } from 'mobx-react';
 import styles from './index.module.scss';
 import { Icon } from '@discuzq/design';
 import Router from '@discuzq/sdk/dist/router';
 import browser from '@common/utils/browser';
 import UnreadRedDot from '@components/unread-red-dot';
+import { unreadUpdateInterval } from '@common/constants/message';
 
 const H5Header = (props) => {
-  const { allowJump = true, customJum = () => { }, message: { totalUnread } } = props;
+  const { allowJump = true, customJum = () => { }, message: { totalUnread, readUnreadCount }, user } = props;
   // todo
   const iconClickHandle = useCallback((link) => {
     if (allowJump) {
       Router.push({ url: link });
-      return
-    };
+      return;
+    }
     customJum(link);
   }, []);
 
   const goBackClickHandle = useCallback(() => {
     if (allowJump) {
       window.history.length <= 1 ? Router.redirect({ url: '/' }) : Router.back();
-      return
+      return;
     };
     customJum();
+  }, []);
+
+  // 轮询更新未读消息
+  const timeoutRef = useRef();
+  const updateUnreadMessage = () => {
+    if (!user.id) return;
+    readUnreadCount();
+    timeoutRef.current = setTimeout(() => {
+      updateUnreadMessage();
+    }, unreadUpdateInterval);
+  };
+
+  useEffect(() => {
+    updateUnreadMessage();
+    return () => clearTimeout(timeoutRef.current);
   }, []);
 
   return (
@@ -60,4 +76,4 @@ const H5Header = (props) => {
   );
 }
 
-export default inject("message")(observer(H5Header));
+export default inject("message", 'user')(observer(H5Header));
