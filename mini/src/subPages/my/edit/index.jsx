7c193c0aba6eb6808c2250a893e0d35ef8c2c449
jf19981111 <18270711420@163.com>
2021-06-25 15:10:55 +0800

    fix: 修复编辑资料报错

--- mini/src/subPages/my/edit/index.jsx
+++ mini/src/subPages/my/edit/index.jsx
@@ -1,6 +1,7 @@
 import Page from '@components/page';
 import React, { useEffect } from 'react';
 import UserCenterEditInfo from '@components/user-center-edit-info';
+import Taro from '@tarojs/taro';
 
 export default function index() {
   useEffect(() => {
