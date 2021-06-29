d285db01aafcb3670c7ff08088000660e8c4ea90
leohaozhang <leohaozhang@tencent.com>
2021-06-29 16:35:03 +0800

    fix: 修复css报顺序异常的warning

--- web/components/loading-box/index.jsx
+++ web/components/loading-box/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from './index.module.scss';
 import { Spin } from '@discuzq/design';
+import styles from './index.module.scss';
 
 export default function LoadingBox(props) {
   return (
