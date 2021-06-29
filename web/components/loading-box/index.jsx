01b2d39f1b71cdc387809ccc4f189d9b884b22a0
leohaozhang <leohaozhang@tencent.com>
2021-06-29 19:05:56 +0800

    revert

--- web/components/loading-box/index.jsx
+++ web/components/loading-box/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Spin } from '@discuzq/design';
 import styles from './index.module.scss';
+import { Spin } from '@discuzq/design';
 
 export default function LoadingBox(props) {
   return (
