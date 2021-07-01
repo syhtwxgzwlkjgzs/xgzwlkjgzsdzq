4918792f822cc704b48d2bf8af54e10d6464fe8b
lizhencui <927117360@qq.com>
2021-07-01 16:06:01 +0800

    fix: 帖子审核中时，审核的样式遮挡住右上角下拉菜单

--- mini/src/layout/thread/index.jsx
+++ mini/src/layout/thread/index.jsx
@@ -1026,12 +1026,14 @@ class ThreadH5Page extends React.Component {
             ></RewardPopup>
 
             {/* 采纳弹层 */}
-            <AboptPopup
-              rewardAmount={parseContent?.REWARD?.money} // 需要传入剩余悬赏金额
-              visible={this.state.showAboptPopup}
-              onCancel={() => this.onAboptCancel()}
-              onOkClick={(data) => this.onAboptOk(data)}
-            ></AboptPopup>
+            {parseContent?.REWARD?.money && (
+              <AboptPopup
+                rewardAmount={parseContent?.REWARD?.money} // 需要传入剩余悬赏金额
+                visible={this.state.showAboptPopup}
+                onCancel={() => this.onAboptCancel()}
+                onOkClick={(data) => this.onAboptOk(data)}
+              ></AboptPopup>
+            )}
           </Fragment>
         )}
       </View>
