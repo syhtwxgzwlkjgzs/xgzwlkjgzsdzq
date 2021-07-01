549a10c120c4507338d881e0e964a17f3003b7b1
joneeeeli <joneeeeli@tencent.com>
2021-07-01 11:58:05 +0800

    fix

--- common/utils/calc-cos-image-quality/adapter/taro.js
+++ common/utils/calc-cos-image-quality/adapter/taro.js
@@ -3,19 +3,16 @@ import Taro from '@tarojs/taro';
 
 export default function taro(src, type, level) {
     const [path, param] = src.split('?');
-    let newSrc = src;
-    let newParam = '';
     const info = Taro.getSystemInfoSync();
     const viewWidth = info.windowWidth;
-    newParam = calcImageQuality(viewWidth, type, level);
+    let paramArr = [];
+    const newParam = calcImageQuality(viewWidth, type, level);
 
-    if ( param && param !== '' ) {
-        const paramArr = param.split('&');
-        paramArr.push(newParam);
-        newSrc = `${newSrc}&${paramArr.join('&')}`;
-    } else {
-        newSrc = `${newSrc}?${newParam}`;
+    if (param && param !== '') {
+        paramArr = param.split('&');
     }
+    paramArr.push(newParam);
+    const newSrc = `${path}?${paramArr.join('&')}`;
 
     return newSrc;
 }
