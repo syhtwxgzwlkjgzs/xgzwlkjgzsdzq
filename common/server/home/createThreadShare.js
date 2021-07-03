6d27e9f121517dd3f05d4dafbaf0f8f8f7a6b3a0
Sean Luo <luoxinran@eims.com.cn>
2021-07-03 10:00:22 +0800

    create-thread-share api引入

--- common/server/home/createThreadShare.js
+++ common/server/home/createThreadShare.js
@@ -1,26 +1,34 @@
+import { createThreadShare } from '@discuzq/sdk/dist/api/home/create-thread-share';
 
-// import {createInviteLink} from '@discuzq/sdk/dist/api/invite/create-invite-link';
-import api from '../api';
-// export default async function share(opts, ctx = null) {
-//   // , url: '/apiv3/sticks'
-//   const res = await readStickList({ ...opts, __context: ctx, url: '/apiv3/thread.share' });
-//   return res;
-// }
-
-export default async function createThreadShare(opts, ctx = null) {
-  try {
-    const { params = {}, data = {}, ...others } = opts;
-    const options = {
-      url: '/apiv3/thread.share', // 请求地址
-      method: 'POST',
-      params,
-      data,
-      __context: ctx,
-      ...others,
-    };
-    const result = await api.http(options);
-    return result;
-  } catch (error) {
-    return error;
-  }
+export default async function _createThreadShare(opts, ctx = null) {
+  const res = await createThreadShare({ ...opts, __context: ctx });
+  return res;
 }
+
+
+
+// // import {createInviteLink} from '@discuzq/sdk/dist/api/invite/create-invite-link';
+// import api from '../api';
+// // export default async function share(opts, ctx = null) {
+// //   // , url: '/apiv3/sticks'
+// //   const res = await readStickList({ ...opts, __context: ctx, url: '/apiv3/thread.share' });
+// //   return res;
+// // }
+
+// export default async function createThreadShare(opts, ctx = null) {
+//   try {
+//     const { params = {}, data = {}, ...others } = opts;
+//     const options = {
+//       url: '/apiv3/thread.share', // 请求地址
+//       method: 'POST',
+//       params,
+//       data,
+//       __context: ctx,
+//       ...others,
+//     };
+//     const result = await api.http(options);
+//     return result;
+//   } catch (error) {
+//     return error;
+//   }
+// }
