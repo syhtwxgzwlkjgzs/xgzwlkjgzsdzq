d3e7400c9eb0d6b424ec579cc89f4139e2bbcf6f
joneeeeli <joneeeeli@tencent.com>
2021-07-05 23:24:22 +0800

    本地图片修改为远程图片

--- mini/src/subPages/no-install/index.jsx
+++ mini/src/subPages/no-install/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, Image } from '@tarojs/components';
 import Page from '@components/page';
 import styles from './index.module.scss';
-import imgError from '../../../../web/public/dzq-img/error.png';
+// import imgError from '../../../../web/public/dzq-img/error.png';
 
 class SiteError extends Component {
   componentWillMount() { }
@@ -19,7 +19,7 @@ class SiteError extends Component {
     return (
       <Page>
         <View className={styles.page}>
-          <Image className={styles.img} src={imgError}/>
+          <Image className={styles.img} src='https://imgcache.qq.com/operation/dianshi/other/error.6332cffff6f7fcc0a193a12a7eb74cab05332bba.png'/>
           <Text className={styles.text}>站点未安装</Text>
         </View>
       </Page>
