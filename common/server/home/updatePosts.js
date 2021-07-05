f15858acb73a35a64b829b17915285657a452598
Sean Luo <luoxinran@eims.com.cn>
2021-07-05 15:55:48 +0800

    包装home和search的apiv3的API

--- common/server/home/updatePosts.js
+++ common/server/home/updatePosts.js
@@ -1,11 +1,10 @@
-import {updatePosts} from '@discuzq/sdk/dist/api/content/update-posts';
+import { updatePosts } from '@discuzq/sdk/dist/api/home/update-posts';
 
-
-/**
- * 点赞
+/** 修改评论接口
+ * @param {object} params
+ * @returns object
  */
-export default async function _updatePosts(opt = {}, ctx = null) {
-  const res = await updatePosts({ ...opt, __context: ctx, url: '/apiv3/posts.update', isValidate: false });
-
+export default async function _updatePosts(opts, ctx = null) {
+  const res = await updatePosts({ ...opts, __context: ctx, isValidate: false });
   return res;
 }
