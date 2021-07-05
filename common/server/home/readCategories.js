d0d2f087a585c05dbe18c8ea9241d187b7ebf223
Sean Luo <luoxinran@eims.com.cn>
2021-07-05 08:58:51 +0800

    修改分享、分类、点赞、推荐、置顶的API

--- common/server/home/readCategories.js
+++ common/server/home/readCategories.js
@@ -1,5 +1,10 @@
-import {readCategories} from '@discuzq/sdk/dist/api/content/read-categories';
-export default async function _readCategories(opts, ctx) {
-  const res = await readCategories({ ...opts, __context: ctx, url: '/apiv3/categories' });
+import { readCategories } from '@discuzq/sdk/dist/api/home/create-thread-share';
+
+/** 创建帖子分享
+ * @param {object} params
+ * @returns object
+ */
+export default async function _readCategories(opts, ctx = null) {
+  const res = await readCategories({ ...opts, __context: ctx });
   return res;
 }
