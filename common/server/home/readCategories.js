40bacb3b6d349bb48bf446bd081f5410e8458ab3
Sean Luo <luoxinran@eims.com.cn>
2021-07-05 15:55:48 +0800

    包装home和search的apiv3的API

--- common/server/home/readCategories.js
+++ common/server/home/readCategories.js
@@ -1,5 +1,10 @@
-import {readCategories} from '@discuzq/sdk/dist/api/content/read-categories';
-export default async function _readCategories(opts, ctx) {
-  const res = await readCategories({ ...opts, __context: ctx, url: '/apiv3/categories' });
+import { readCategories } from '@discuzq/sdk/dist/api/home/read-categories';
+
+/** 获取分类列表接口
+ * @param {object} params
+ * @returns object
+ */
+export default async function _readCategories(opts, ctx = null) {
+  const res = await readCategories({ ...opts, __context: ctx });
   return res;
 }
--- common/server/home/readStickList.js
+++ common/server/home/readStickList.js
@@ -1,7 +1,10 @@
-import {readStickList} from '@discuzq/sdk/dist/api/content/read-sticklist';
+import { readStickList } from '@discuzq/sdk/dist/api/home/read-stick-list';
 
-
+/** 置顶列表
+ * @param {object} params
+ * @returns object
+ */
 export default async function _readStickList(opts, ctx = null) {
-  const res = await readStickList({ ...opts, __context: ctx, url: '/apiv3/thread.stick' });
+  const res = await readStickList({ ...opts, __context: ctx });
   return res;
 }
