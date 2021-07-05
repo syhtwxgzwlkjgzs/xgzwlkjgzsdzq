e50248bf39aea567fadfd4c5b299a597446ce4b4
leohaozhang <leohaozhang@tencent.com>
2021-07-05 16:53:10 +0800

    fix: 登录注册相关代码迁移到sdk

--- common/server/forum/groupPermissionList.js
+++ common/server/forum/groupPermissionList.js
@@ -1,24 +1,6 @@
-// import {readCategories} from '@discuzq/sdk/dist/api/content/read-categories';
-import api from '../api';
+import {groupPermissionList} from '@discuzq/sdk/dist/api/forum/group-permission-list';
 
-/** 当前用户所拥有的权限
- * @param {object} params
- * @returns object
- */
-export default async function groupPermissionList(opts = {}, ctx = null) {
-  try {
-    const { params = {}, data = {}, ...others } = opts;
-    const options = {
-      url: '/apiv3/group.permission.list', // 请求地址
-      method: 'GET',
-      params,
-      data,
-      __context: ctx,
-      ...others,
-    };
-    const result = await api.http(options);
-    return result;
-  } catch (error) {
-    return error;
-  }
-}
+export default async function _groupPermissionList(opts, ctx) {
+  const res = await groupPermissionList({ ...opts, __context: ctx });
+  return res;
+};
