4a12adc7a1aed5eb64897b1204745d71e08bb5aa
Sean Luo <luoxinran@eims.com.cn>
2021-07-07 10:33:59 +0800

    UserInfo时间截取

--- web/components/thread/index.jsx
+++ web/components/thread/index.jsx
@@ -237,7 +237,7 @@ class Index extends React.Component {
                 location={position.location}
                 view={`${viewCount}`}
                 groupName={group?.groupName}
-                time={diffTime.substr(0, 10)}
+                time={diffTime}
                 isEssence={isEssence}
                 isPay={isPrice}
                 isRed={isRedPack}
