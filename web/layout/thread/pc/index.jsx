e4989be91d9aa4abbe6f5c2887aa961598f8440a
kevinchou <kevinchou@tencent.com>
2021-07-12 15:12:00 +0800

    fix: 调整详情右侧布局样式

--- web/layout/thread/pc/index.jsx
+++ web/layout/thread/pc/index.jsx
@@ -777,7 +777,10 @@ class ThreadPCPage extends React.Component {
     //   // newText = newText.replace(/\n/g, '');
     //   console.log(newText);
     // }
+    // 是否匿名
     const isAnonymous = threadStore?.threadData?.isAnonymous;
+    // 是否作者本人
+    const isSelf = this.props.user?.userInfo?.id && this.props.user?.userInfo?.id === threadStore?.threadData?.userId;
     return (
       <div>
         <ShowTop showContent={this.props.thread?.threadData?.isStick} setTop={this.state.setTop}></ShowTop>
@@ -794,6 +797,7 @@ class ThreadPCPage extends React.Component {
           ready={() => this.onBaseLayoutReady()}
           rightClassName={classNames(layout.positionSticky, {
             'is-userinfo-show': !isAnonymous,
+            'is-operate-show': !isSelf,
           })}
           className="detail"
         >
