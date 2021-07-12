fa88a0e556c1be6903640fec596260cc043e3c0d
leohaozhang <leohaozhang@tencent.com>
2021-07-12 17:36:48 +0800

    update: 更新登录页备案信息

--- web/layout/user/h5/components/pc-body-wrap/index.jsx
+++ web/layout/user/h5/components/pc-body-wrap/index.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import { inject } from 'mobx-react';
 import layout from './index.module.scss';
 import BaseLayout from '@components/base-layout';
+import CopyRight from '@components/copyright';
 
 @inject('site')
 class PcBodyWrap extends React.Component {
   render() {
     const { children, site } = this.props;
     const { platform } = site;
+
     if (platform === 'h5') {
       return <div>{children}</div>;
     }
+
     return (
       <BaseLayout>
         <div className={layout.pc_body_background}>
           {children}
-          <div className={layout.pc_bottom}>{site?.webConfig?.setSite?.siteRecord}</div>
+          <CopyRight center line />
         </div>
       </BaseLayout>
     );
