0e39248c16c4649eb17e000a163f0df70edaac2d
leohaozhang <leohaozhang@tencent.com>
2021-07-13 21:27:07 +0800

    update: 站点加入付费内容优化，及续费参数调整

--- common/store/search/action.js
+++ common/store/search/action.js
@@ -213,8 +213,22 @@ class SearchAction extends SearchStore {
  * @returns {object} 处理结果
  */
  @action
-  async getThreadList({ search = '', perPage = 10, page = 1, params = {} } = {}) {
-    const result = await readThreadList({ params: { sequence: '0', filter: { sort: '3', search }, perPage, page, ...params } });
+  async getThreadList({ search = undefined, isSite = undefined, removeThreadIds = undefined, perPage = 10, page = 1, params = {} } = {}) {
+    const payload = {
+      params: {
+        sequence: '0',
+        filter: {
+          sort: '3',
+          search,
+          isSite,
+          removeThreadIds
+        },
+        perPage,
+        page,
+        ...params
+      }
+    }
+    const result = await readThreadList(payload);
 
     if (result.code === 0 && result.data) {
       if (this.threads && result.data.pageData && page !== 1) {
