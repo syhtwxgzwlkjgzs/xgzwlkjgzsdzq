830ce7bc843cac2c13ccb30e84f058fcd26b86b5
Sean Luo <luoxinran@gmail.com>
2021-07-13 10:18:23 +0800

    增加判断

--- common/utils/viewed-in-storage.js
+++ common/utils/viewed-in-storage.js
@@ -1,12 +1,13 @@
 /**
- * localStorage中存储页面帖子被观看过的信息，用于记录分析浏览量等信息
+ * sessionStorage中存储页面帖子被观看过的信息，用于记录分析浏览量等信息
  *
  * @param  {number}    threadId: 帖子的ID
  *
  */
 const isViewed = function (threadId = null) {
   if(!threadId) return false;
-  const viewedThreads = localStorage.getItem("viewed");
+  threadId += ''; // 统一为字符串
+  const viewedThreads = sessionStorage.getItem("viewed"); // TODO: 用sessionStorage
   for(const thread of viewedThreads) {
     if(thread.id === threadId) return true;
   }
@@ -30,13 +31,15 @@ const isViewed = function (threadId = null) {
  * }
  */
 const addViewed = function (threadId = null) {
-  if(!threadId) return ;
-  const viewedThreads = localStorage.getItem("viewed");
+  if(!threadId) return ; // TODO: threadId可能是number或string
+  threadId += ''; // 统一为字符串
+  if(isViewed(threadId)) return;
+  const viewedThreads = sessionStorage.getItem("viewed");
   viewedThreads.push({
     id: threadId,
     viewedAt: new Date().getTime(),
   });
-  localStorage.setItem("viewed", { threads: viewedThreads });
+  sessionStorage.setItem("viewed", { threads: viewedThreads });
 }
 
 
