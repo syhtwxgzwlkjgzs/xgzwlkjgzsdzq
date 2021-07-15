c8629920fd2f43c6e15bec75787fc9a9e09d06aa
Sean Luo <luoxinran@gmail.com>
2021-07-15 12:49:08 +0800

    跳转前等待store更新完成

--- web/components/thread/post-content/index.jsx
+++ web/components/thread/post-content/index.jsx
@@ -94,6 +94,8 @@ const PostContent = ({
   // 点击富文本中的链接
   const handleLinkClick = () => {
     updateViewCount();
+    setTimeout(() => { // 等待store更新完成后跳转
+    }, 500);
   }
 
   // 超过1200个字符，截断文本用于显示
