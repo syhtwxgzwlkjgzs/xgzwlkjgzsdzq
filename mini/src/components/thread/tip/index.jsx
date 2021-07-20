2783fa4642ec825b640d5ec6aa174b5844571615
kevinchou <kevinchou@tencent.com>
2021-07-20 16:10:33 +0800

    fix: 修改点赞列表更多图片的定位

--- mini/src/components/thread/tip/index.jsx
+++ mini/src/components/thread/tip/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo } from 'react';
 import PopupList from '../popup-list';
 import Avatar from '../../avatar';
 import { View, Text, Image } from '@tarojs/components';
-import Icon from '@discuzq/design/dist/components/icon/index'
+import Icon from '@discuzq/design/dist/components/icon/index';
 import styles from './index.module.scss';
 import { inject, observer } from 'mobx-react';
 import { debounce } from '@common/utils/throttle-debounce.js';
@@ -12,68 +12,66 @@ import { debounce } from '@common/utils/throttle-debounce.js';
  * @prop {string}    imgs 头像数据数组
  */
 
- const Index = inject('index')(
-  observer(({ imgs = [], tipData = {}, wholeNum = 1,showMore=false, index, showCount = 5, unifyOnClick = null }) => {
-  const [visible, setVisible] = useState(false);
+const Index = inject('index')(
+  observer(({ imgs = [], tipData = {}, wholeNum = 1, showMore = false, index, showCount = 5, unifyOnClick = null }) => {
+    const [visible, setVisible] = useState(false);
 
-  const onClick = debounce((e) => {
-    e.stopPropagation();
-    index.setHiddenTabBar(true)
-    index.setHasOnScrollToLower(false)
-    setVisible(true);
-  }, 200);
+    const onClick = debounce((e) => {
+      e.stopPropagation();
+      index.setHiddenTabBar(true);
+      index.setHasOnScrollToLower(false);
+      setVisible(true);
+    }, 200);
 
-  const onHidden = () => {
-    index.setHiddenTabBar(false)
-    index.setHasOnScrollToLower(true)
+    const onHidden = () => {
+      index.setHiddenTabBar(false);
+      index.setHasOnScrollToLower(true);
 
-    setVisible(false);
-  };
+      setVisible(false);
+    };
 
-  const renderUsers = useMemo(() => {
-    const map = {};
-    return imgs.reduce((result, item) => {
-      if (!map[item.userId]) {
-        result.push(item);
-        map[item.userId] = 1;
-      }
-      return result;
-    }, [])
-  }, [imgs]);
+    const renderUsers = useMemo(() => {
+      const map = {};
+      return imgs.reduce((result, item) => {
+        if (!map[item.userId]) {
+          result.push(item);
+          map[item.userId] = 1;
+        }
+        return result;
+      }, []);
+    }, [imgs]);
 
     // 点赞头像的总宽度
-  const sty = useMemo(() => {
-    const imgsLength = renderUsers.length;
-    return { width: `${(16*imgsLength+4*(imgsLength+1))*2}rpx`}
-  }, [renderUsers]);
+    const sty = useMemo(() => {
+      const imgsLength = renderUsers.length;
+      return { width: `${(16 * imgsLength + 4 * (imgsLength + 1)) * 2}rpx` };
+    }, [renderUsers]);
 
-  const imgAfterArr = [styles.img, styles.imgAfter2, styles.imgAfter3, styles.imgAfter4, styles.imgAfter5];
+    const imgAfterArr = [styles.img, styles.imgAfter2, styles.imgAfter3, styles.imgAfter4, styles.imgAfter5];
 
     return (
-    <>
+      <>
         <View className={styles.container} onClick={unifyOnClick || onClick} style={sty}>
-            {
-                wholeNum !== 0 && renderUsers?.filter((_, index) => index < showCount).map((item, index) => (
-                  <View key={index} className={imgAfterArr[index]}>
-                    <Avatar
-                      image={item.avatar}
-                      name={item.nickname}
-                      size='small'
-                    />
-                  </View>
-                ))
-            }
-            {
-              showMore && renderUsers?.length > showCount &&
-              <View className={styles.moreIcon} size={20}>
-                <Icon name='MoreBOutlined' className={styles.icon} size={12}></Icon>
-              </View>
-            }
+          {wholeNum !== 0 &&
+            renderUsers
+              ?.filter((_, index) => index < showCount)
+              .map((item, index) => (
+                <View key={index} className={imgAfterArr[index]}>
+                  <Avatar image={item.avatar} name={item.nickname} size="small" />
+
+                  {showMore && renderUsers?.length > showCount && index === 4 && (
+                    <View className={styles.moreIcon} size={20}>
+                      <Icon name="MoreBOutlined" className={styles.icon} size={12}></Icon>
+                    </View>
+                  )}
+                </View>
+              ))}
         </View>
 
         {visible && <PopupList tipData={tipData} visible={visible} onHidden={onHidden} />}
-    </>
-  );
-}));
+      </>
+    );
+  }),
+);
 
 export default React.memo(Index);
