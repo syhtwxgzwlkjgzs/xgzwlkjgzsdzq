1ac736a91cbcc8d50ce6464fb114d7f725b19fc0
leohaozhang <leohaozhang@tencent.com>
2021-07-22 21:37:37 +0800

    update：调整copyright组件样式

--- web/components/copyright/index.jsx
+++ web/components/copyright/index.jsx
@@ -9,22 +9,46 @@ class CopyRight extends React.Component {
   constructor(props) {
     super(props)
   }
+
   render() {
     const { site, center, line } = this.props;
     if ( !site || !site.webConfig || !site.webConfig.setSite) return null;
+
     const { setSite } = site.webConfig;
+
+    const { platform } = site;
+    const isH5 = platform === 'h5';
+
     const clsName = classnames(styles.container, {
-      [`${styles.center}`]: center,
-      [`${styles.line}`]: line,
       copyright: true,
+      [styles.center]: center || isH5,
+      [styles.h5]: isH5
     })
+
+    const curSiteUrl = window.location.origin;
+    const curYear = new Date().getFullYear();
+
     return (
       <div className={clsName}>
-        {setSite.siteRecord && <div className={styles.text}><a href="https://beian.miit.gov.cn/" target="_blank">{setSite.siteRecord}</a></div>}
-        {setSite.siteRecordCode && <div className={styles.text}><a
-          href={`http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=${setSite.siteRecordCode}`}
-          target="_blank"
-        >{setSite.siteRecordCode}</a></div>}
+        <div className={styles.text}>
+          <a href="https://discuz.chat" target="_blank">
+            Powered By Discuz！Q
+          </a>
+        </div>
+        <div className={styles.text}>
+          <a href={curSiteUrl} target="_blank">
+            &copy; {curYear} {setSite.siteName}
+          </a>
+        </div>
+        {
+          setSite.siteRecordCode && (
+            <div className={styles.text}>
+              <a href={`http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=${setSite.siteRecordCode}`} target="_blank">
+                {setSite.siteRecordCode}
+              </a>
+            </div>
+          )
+        }
       </div>
     );
   }
