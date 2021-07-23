84274abc7310763584b5ca89bacd845f30545e65
刘宇琳 <burnellliu>
2021-07-23 20:27:52 +0800

    fix: 删除冗余代码

--- web/components/user-center/additional-info/index.jsx
+++ web/components/user-center/additional-info/index.jsx
@@ -3,7 +3,6 @@ import styles from './index.module.scss';
 import { Spin, Input, Icon, Dialog, Toast, Button } from '@discuzq/design';
 import { inject, observer } from 'mobx-react';
 import Header from '@components/header';
-import additionalInfoData from '../additional-info-pc/test.json';
 @inject('user')
 @observer
 export default class UserCenterAdditionalInfo extends Component {
