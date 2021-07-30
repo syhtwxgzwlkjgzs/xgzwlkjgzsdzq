3a7d55e424e129eb763d08be6d354b4aae472b0b
lizhencui <927117360@qq.com>
2021-07-30 15:51:38 +0800

    fix: 悬赏发放的金额与总金额不一致

--- common/utils/calculate.js
+++ common/utils/calculate.js
@@ -18,13 +18,35 @@ export const plus = (num1, num2) => {
 
   const p = p1 > p2 ? p1 : p2;
 
-  const n1 = (num1 * Math.pow(10, p)) | 0;
-  const n2 = (num2 * Math.pow(10, p)) | 0;
+  // const n1 = (num1 * Math.pow(10, p)) | 0;
+  // const n2 = (num2 * Math.pow(10, p)) | 0;
+  const n1 = (accMul(num1, p)) | 0;
+  const n2 = (accMul(num2, p)) | 0;
 
   const result = (n1 + n2) / Math.pow(10, p);
 
   return result.toFixed(2);
 };
+
+// 含小数的数与Math.pow(10, p)相乘时会出现丢失精度现象
+const accMul = (num, p) => {
+  if (num.toString().split('.').length <= 1) return num * Math.pow(10, p);
+
+  const numLength = num.toString().split('.')[1].length;
+  let numberStr = num.toString();
+  let result = null;
+
+  if (p === numLength) {
+      result = Number(numberStr.replace('.',''));
+      return result;
+  } else {
+      for(let i = 0; i < p - numLength; i++) {
+          numberStr += '0';
+      }
+      result = Number(numberStr.replace('.',''));
+      return result;
+  }
+}
 
 export const minus = (num1, num2) => {
   if (isNaN(num1) || isNaN(num2)) return 0;
