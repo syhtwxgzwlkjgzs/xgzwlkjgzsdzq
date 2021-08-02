e5ce10f967c2e38e8f39d5653b17076cf2c82018
Sean Luo <luoxinran@gmail.com>
2021-08-02 19:36:18 +0800

    小程序行宽减半

--- mini/src/components/thread/post-content/index.jsx
+++ mini/src/components/thread/post-content/index.jsx
@@ -117,7 +117,7 @@ import { urlToLink } from '@common/utils/replace-url-to-a';
   };
 
   useEffect(() => {
-    const lengthInLine = parseInt((contentWrapperRef.current.offsetWidth || 704) / 16);
+    const lengthInLine = parseInt((contentWrapperRef.current.offsetWidth || 704) / 32);
 
     const length = fuzzyCalcContentLength(filterContent, lengthInLine); // 大致计算文本长度
     const maxContentLength = lengthInLine * 6; // 如果默认长度是704，一共可容纳264个字符
