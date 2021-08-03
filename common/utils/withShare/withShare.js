5f691d9ca37ecf596b4ba57ba9f8dc78f3eef9c9
jinyuuzhang <jinyuuzhang@tencent.com>
2021-08-03 15:32:00 +0800

    修复帖子详情页面匿名分享，以及灰色遮罩层问题

--- common/utils/withShare/withShare.js
+++ common/utils/withShare/withShare.js
@@ -1,23 +1,18 @@
 import Taro from '@tarojs/taro';
-import Toast from '@discuzq/design/dist/components/toast';
-import goToLoginPage from '@common/utils/go-to-login-page';
-import { inject, observer } from 'mobx-react';
+
 /**
  * @param {boolean} needShareline 是否需要分享到朋友圈
  * @param {boolean} needLogin 是否需要登录
  * @returns
  */
-function withShare(opts = {}) {
+function withShare() {
   // 设置默认
   const defalutTitle = 'Discuz!Q';
   let defalutPath = 'pages/index/index';
   defalutPath = `/pages/index/index?path=${encodeURIComponent(defalutPath)}`;
   let menus = [];
-  const { needShareline = true, needLogin = true } = opts;
   menus = ['shareAppMessage'];
   return function demoComponent(Component) {
-    @inject('user')
-    @observer
     class WithShare extends Component {
       componentDidMount() {
         Taro.showShareMenu({
@@ -29,16 +24,6 @@ function withShare(opts = {}) {
         }
       }
       onShareAppMessage = (res) => {
-        const { user } = this.props;
-        // 是否必须登录
-        if (needLogin && !user.isLogin()) {
-          Toast.info({ content: '请先登录!' });
-          goToLoginPage({ url: '/subPages/user/wx-auth/index' });
-          const promise = Promise.reject();
-          return {
-            promise,
-          };
-        }
         const data = res.target?.dataset?.shareData || '';
         let shareData = '';
         if (this.getShareData && typeof this.getShareData === 'function') {
@@ -46,11 +31,16 @@ function withShare(opts = {}) {
         }
         const { title = defalutTitle, path = defalutPath, imageUrl = '' } = shareData;
         const encodePath = `/pages/index/index?path=${encodeURIComponent(path)}`;
-        return {
+        const value = {
           title,
           path: encodePath,
           imageUrl,
         };
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(value);
+          }, 1000);
+        });
       }
 
       render() {
