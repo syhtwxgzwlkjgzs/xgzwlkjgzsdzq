61616a05b4ccd3a90d25eb6616b77c8f13efa73b
ljf <18270711420@163.com>
2021-08-04 17:00:23 +0800

    fix: user页面重定向

--- web/pages/user/index.jsx
+++ web/pages/user/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import styles from './index.module.scss';
+// import styles from './index.module.scss';
+import Redirect from '@components/redirect';
 export default class Detail extends React.Component {
   render() {
     return (
-      <div className='index'>
-        <h1>user</h1>
-        <p className={styles.text}>33333</p>
-      </div>
+      <Redirect jumpUrl={'/my'} />
     );
   }
 }
